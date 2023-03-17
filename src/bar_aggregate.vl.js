14d42c071f690be98e1c509b84d4d634b2a8fede
mhkeller <code@mhkeller.com>
2023-03-16 22:23:27 -0400

    format

--- src/bar_aggregate.vl.js
+++ src/bar_aggregate.vl.js
@@ -2,15 +2,15 @@ import * as vl from 'vega-lite-api';
 
 export default function chart() {
   return vl.markBar()
-  .height({ step: 17 })
-  .data('data/population.json')
-  .transform(
-    vl.filter('datum.year == 2000')
-  )
-  .encode(
-    vl.y().field('age'),
-    vl.x().aggregate('sum').field('people').title('population')
-  )
+    .height({ step: 17 })
+    .data('data/population.json')
+    .transform(
+      vl.filter('datum.year == 2000')
+    )
+    .encode(
+      vl.y().field('age'),
+      vl.x().aggregate('sum').field('people').title('population')
+    )
     .description('A bar chart showing the US population distribution of age groups in 2000.')
     .toSpec();
 }
