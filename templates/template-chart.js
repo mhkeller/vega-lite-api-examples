7370e6f0aa366bf6bfa53f4051fb0c240bf19036
mhkeller <code@mhkeller.com>
2023-04-14 00:49:53 -0400

    update template

--- templates/template-chart.js
+++ templates/template-chart.js
@@ -3,8 +3,8 @@ import * as vl from 'vega-lite-api';
 /**
  * Write a Node.JS function that uses the vega-lite-api library to
  * generate and return the vega-lite JSON spec below.
- * The function should return the vega-lite-api object.
  */
+
 
 /*
 SPEC
