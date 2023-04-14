77d05eeb87a08822390d334c95050e05f3dbf32a
mhkeller <code@mhkeller.com>
2023-04-13 23:07:43 -0400

    stacked_bar_1d

--- src/stacked_bar_1d.vl.js
+++ src/stacked_bar_1d.vl.js
@@ -1,7 +1,16 @@
 import * as vl from 'vega-lite-api';
 
 // Write a Node.JS function that uses the vega-lite-api library to generate the vega-lite JSON spec below. The function should return the vega-lite-api object. Call the function `chart` and export as the es6 module default export.
-
+export default function chart () {
+	return vl
+		.markBar()
+		.data('data/cars.json')
+		.encode(
+			vl.x().aggregate('sum').field('Acceleration'),
+			vl.color().field('Origin')
+		)
+		.toSpec();
+}
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
---
+++ test/stacked_bar_1d.test.js
@@ -0,0 +1,21 @@
+/* globals describe it */
+import * as assert from 'assert';
+import { readFileSync } from 'fs';
+
+import fn from '../src/stacked_bar_1d.vl.js';
+
+const name = 'stacked_bar_1d';
+
+const spec = JSON.parse(readFileSync(`./specs/${name}.vl.json`, 'utf-8'));
+
+// Change this from a string to an object, which is what vega-lite-api creates
+if (typeof spec.mark === 'string') {
+	spec.mark = {type: spec.mark }
+}
+
+describe(`${name}.vg.js`, () => {
+	it(`should match spec`, () => {
+		const actual = fn();
+		assert.deepStrictEqual(actual, spec);
+	});
+});
