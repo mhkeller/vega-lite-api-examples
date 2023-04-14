3ebda20c64e48544244eec55cccdd602e675df75
mhkeller <code@mhkeller.com>
2023-04-14 00:45:28 -0400

    cleanup

--- src/stacked_bar_1d.vl.js
+++ src/stacked_bar_1d.vl.js
@@ -2,7 +2,7 @@ import * as vl from 'vega-lite-api';
 
 /**
  * Write a Node.JS function that uses the vega-lite-api library to
- * generate the vega-lite JSON spec below.
+ * generate and return the vega-lite JSON spec below.
  * The function should return the vega-lite-api object.
  */
 export default function chart () {
