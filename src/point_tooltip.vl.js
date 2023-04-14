0bc5202235da7fdb6bb036c763eb9c589ee35f63
mhkeller <code@mhkeller.com>
2023-04-13 23:05:32 -0400

    point_tooltip

--- src/point_tooltip.vl.js
+++ src/point_tooltip.vl.js
@@ -1,7 +1,18 @@
 import * as vl from 'vega-lite-api';
 
 // Write a Node.JS function that uses the vega-lite-api library to generate the vega-lite JSON spec below. The function should return the vega-lite-api object. Call the function `chart` and export as the es6 module default export.
-
+export default function chart () {
+	return vl
+		.markPoint()
+		.description('A scatterplot showing horsepower and miles per gallons for various cars.')
+		.data('data/cars.json')
+		.encode(
+			vl.x().fieldQ('Horsepower'),
+			vl.y().fieldQ('Miles_per_Gallon'),
+			vl.tooltip().fieldN('Origin')
+		)
+		.toSpec();
+}
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
