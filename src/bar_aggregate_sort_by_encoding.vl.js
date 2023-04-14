0d1d1a8c4bd4de5237b1391c96bf6130c7d02206
mhkeller <code@mhkeller.com>
2023-04-13 22:59:30 -0400

    bar_aggregate_sort_mean

--- src/bar_aggregate_sort_by_encoding.vl.js
+++ src/bar_aggregate_sort_by_encoding.vl.js
@@ -1,7 +1,19 @@
 import * as vl from 'vega-lite-api';
 
 // Write a Node.JS function that uses the vega-lite-api library to generate the vega-lite JSON spec below. The function should return the vega-lite-api object. Call the function `chart` and export as the es6 module default export.
-
+export default function chart () {
+	return vl
+		.markBar()
+		.description('A bar chart that sorts the y-values by the x-values.')
+		.data('data/population.json')
+		.transform(vl.filter('datum.year == 2000'))
+		.height({ step: 17 })
+		.encode(
+			vl.x().field('people').aggregate('sum').title('population'),
+			vl.y().fieldO('age').sort('-x')
+		)
+		.toSpec();
+}
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
