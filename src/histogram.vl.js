aa1bbce2585b23407a61bcc974a290ba2d38bd31
mhkeller <code@mhkeller.com>
2023-04-13 23:00:24 -0400

    histogram

--- src/histogram.vl.js
+++ src/histogram.vl.js
@@ -1,7 +1,16 @@
 import * as vl from 'vega-lite-api';
 
 // Write a Node.JS function that uses the vega-lite-api library to generate the vega-lite JSON spec below. The function should return the vega-lite-api object. Call the function `chart` and export as the es6 module default export.
-
+export default function chart () {
+	return vl
+		.markBar()
+		.data('data/movies.json')
+		.encode(
+			vl.x().field('IMDB Rating').bin(true),
+			vl.y().aggregate('count')
+		)
+		.toSpec();
+}
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
