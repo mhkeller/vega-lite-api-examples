36c7f3ad5321a6553bed9f4c94acca9e64115352
mhkeller <code@mhkeller.com>
2023-04-13 23:04:03 -0400

    line_step

--- src/line_step.vl.js
+++ src/line_step.vl.js
@@ -1,7 +1,20 @@
 import * as vl from 'vega-lite-api';
 
 // Write a Node.JS function that uses the vega-lite-api library to generate the vega-lite JSON spec below. The function should return the vega-lite-api object. Call the function `chart` and export as the es6 module default export.
-
+export default function chart () {
+	return vl
+    .markLine({
+      interpolate: 'step-after'
+    })
+		.description('Google\'s stock price over time.')
+		.data('data/stocks.csv')
+		.transform(vl.filter('datum.symbol===\'GOOG\''))
+    .encode(
+      vl.x().fieldT('date'),
+      vl.y().fieldQ('price')
+    )
+		.toSpec();
+}
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
