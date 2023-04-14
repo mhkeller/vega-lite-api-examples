b6af0952930ca9e7634895080b77d0119ae12e2f
mhkeller <code@mhkeller.com>
2023-04-13 22:51:47 -0400

    whitespace

--- src/bar_aggregate_format.vl.js
+++ src/bar_aggregate_format.vl.js
@@ -1,7 +1,19 @@
 import * as vl from 'vega-lite-api';
 
 // Write a Node.JS function that uses the vega-lite-api library to generate the vega-lite JSON spec below. The function should return the vega-lite-api object. Call the function `chart` and export as the es6 module default export.
-
+export default function chart () {
+	return vl
+    .markBar()
+    .description('A bar chart showing the US population distribution of age groups in 2000')
+    .data('data/population.json')
+    .transform(vl.filter('datum.year == 2000'))
+    .height({ step: 17 })
+		.encode(
+			vl.x().field('people').aggregate('sum').title('population').axis({ format: '.2e' }),
+			vl.y().fieldO('age')
+		)
+		.toSpec();
+}
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
