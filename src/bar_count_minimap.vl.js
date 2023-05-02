7cb6ea2ab12e8f8c5c4ceb4efbd2aefb461b6866
mhkeller <code@mhkeller.com>
2023-05-01 22:35:05 -0400

    bar_count_minimap

--- src/bar_count_minimap.vl.js
+++ src/bar_count_minimap.vl.js
@@ -4,7 +4,32 @@ import * as vl from 'vega-lite-api';
  * Write a Node.JS function that uses the vega-lite-api library to
  * generate and return the vega-lite JSON spec below.
  */
-
+export default function chart () {
+	return vl
+		.data('data/cars.json')
+		.hconcat(
+			vl.markBar()
+				.transform([{ filter: { param: 'brush' } }])
+				.encode(
+					vl.y().fieldN('Name').sort('-x').axis({ minExtent: 200, title: null }),
+					vl.x().aggregate('count').axis({ orient: 'top' }).scale({ domain: [0, 6] })
+				),
+			vl.markBar()
+				.width(50)
+				.height(200)
+				.params([{
+					name: 'brush',
+					select: {
+						type: 'interval',
+						encodings: ['y']
+					}
+				}])
+				.encode(
+					vl.y().fieldN('Name').sort('-x').axis(null),
+					vl.x().aggregate('count').axis(null)
+				)
+		).toSpec();
+}
 
 /*
 {
