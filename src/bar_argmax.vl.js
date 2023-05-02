e7b260ea9312638271d963be563c7309151c51f1
mhkeller <code@mhkeller.com>
2023-05-01 21:21:45 -0400

    bar_argmax

--- src/bar_argmax.vl.js
+++ src/bar_argmax.vl.js
@@ -4,7 +4,16 @@ import * as vl from 'vega-lite-api';
  * Write a Node.JS function that uses the vega-lite-api library to
  * generate and return the vega-lite JSON spec below.
  */
-
+export default function chart () {
+	return vl.markBar()
+		.data('data/movies.json')
+		.description('The production budget of the movie that has the highest US Gross in each major genre.')
+		.encode(
+			vl.x().fieldQ('Production Budget').aggregate({ argmax: 'US Gross' }),
+			vl.y().fieldN('Major Genre')
+		)
+		.toSpec();
+}
 
 /*
 {
