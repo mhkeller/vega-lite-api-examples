ef540d641046ae38a4f9954a119f73062a394f1c
mhkeller <code@mhkeller.com>
2023-05-01 23:05:00 -0400

    bar_gantt_config_no_zero

--- src/bar_gantt_config_no_zero.vl.js
+++ src/bar_gantt_config_no_zero.vl.js
@@ -4,7 +4,22 @@ import * as vl from 'vega-lite-api';
  * Write a Node.JS function that uses the vega-lite-api library to
  * generate and return the vega-lite JSON spec below.
  */
-
+export default function chart () {
+	return vl.markBar()
+		.description('A simple bar chart with ranged data (aka Gantt Chart).')
+		.data([
+			{ task: 'A', start: 1, end: 3 },
+			{ task: 'B', start: 3, end: 8 },
+			{ task: 'C', start: 8, end: 10 }
+		])
+		.encode(
+			vl.y().fieldO('task'),
+			vl.x().fieldQ('start'),
+			vl.x2().field('end')
+		)
+		.config({ scale: { zero: false } })
+		.toSpec();
+}
 
 /*
 {
