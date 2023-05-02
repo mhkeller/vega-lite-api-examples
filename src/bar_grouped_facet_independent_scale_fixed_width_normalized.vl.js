8e33371c02e4fee2730816045bd59576a9fcefed
mhkeller <code@mhkeller.com>
2023-05-01 23:39:50 -0400

    bar_grouped_facet_independent_scale_fixed_width_normalized

--- src/bar_grouped_facet_independent_scale_fixed_width_normalized.vl.js
+++ src/bar_grouped_facet_independent_scale_fixed_width_normalized.vl.js
@@ -4,7 +4,34 @@ import * as vl from 'vega-lite-api';
  * Write a Node.JS function that uses the vega-lite-api library to
  * generate and return the vega-lite JSON spec below.
  */
-
+export default function chart() {
+	return vl
+		.data(
+			{
+				values: [
+					{ category: 'A', group: 'x', value: 0.1 },
+					{ category: 'A', group: 'y', value: 0.6 },
+					{ category: 'A', group: 'z', value: 0.9 },
+					{ category: 'B', group: 'x', value: 0.7 },
+					{ category: 'B', group: 'y', value: 0.2 },
+					{ category: 'C', group: 'x', value: 0.6 }
+				]
+			},
+			{ name: 'data' }
+		)
+		.config({ view: { stroke: 'transparent' } })
+		.resolve({ scale: { x: 'independent' } })
+		.facet({ column: { field: 'category', header: { orient: 'bottom' } } })
+		.spec({
+			width: 100,
+			mark: 'bar',
+			encoding: {
+				y: { field: 'value', type: 'quantitative' },
+				x: { field: 'group', axis: null },
+				color: { field: 'group' }
+			}
+		}).toSpec();
+}
 
 /*
 {
