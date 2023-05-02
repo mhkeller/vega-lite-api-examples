f4de352e186c31075c17aafc24cfc8a76a21a62b
mhkeller <code@mhkeller.com>
2023-05-01 22:39:00 -0400

    bar_custom_sort_partial

--- src/bar_custom_sort_partial.vl.js
+++ src/bar_custom_sort_partial.vl.js
@@ -4,7 +4,23 @@ import * as vl from 'vega-lite-api';
  * Write a Node.JS function that uses the vega-lite-api library to
  * generate and return the vega-lite JSON spec below.
  */
-
+export default function chart () {
+	return vl
+		.markBar()
+		.data([
+			{ a: 'A', b: 28 },
+			{ a: 'B', b: 55 },
+			{ a: 'C', b: 43 },
+			{ a: 'Z', b: 91 },
+			{ a: 'Y', b: 81 },
+			{ a: 'X', b: 53 }
+		])
+		.encode(
+			vl.x().fieldO('a').sort(['B', 'A', 'C']),
+			vl.y().fieldQ('b')
+		)
+		.toSpec();
+}
 
 /*
 {
