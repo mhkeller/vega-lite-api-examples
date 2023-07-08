cbd2e65b51ec9637e11d54f3212f086c1bbfc57a
Raymond Turla <raymond.turla@paynamics.net>
2023-07-08 22:17:53 +0800

    arc_ordinal_theta

--- src/arc_ordinal_theta.vl.js
+++ src/arc_ordinal_theta.vl.js
@@ -5,6 +5,26 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+  return vl
+    .markArc()
+    .data([
+      {"dir": "1N", "strength": 1},
+      {"dir": "2NE", "strength": 2},
+      {"dir": "3E", "strength": 3},
+      {"dir": "4SE", "strength": 4},
+      {"dir": "5S", "strength": 5},
+      {"dir": "6SW", "strength": 6},
+      {"dir": "7W", "strength": 7},
+      {"dir": "8NW", "strength": 8}
+    ])
+    .encode(
+      vl.theta().fieldO('dir'),
+      vl.radius().fieldQ('strength'),
+      vl.color().fieldN('dir')
+    )
+    .toSpec();
+}
 
 /*
 {
