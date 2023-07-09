aad12a6c53b8235d13e9364fcc1f3b4cadb947ea
Raymond Turla <raymond.turla@paynamics.net>
2023-07-10 05:09:29 +0800

    area_overlay

--- src/area_overlay_normalized.vl.js
+++ src/area_overlay_normalized.vl.js
@@ -5,6 +5,35 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+  const layer1 = vl
+    .markArea({opacity: 0.7})
+    .encode(
+      vl.x().fieldT('date'),
+      vl.y().fieldQ('price')
+    );
+
+  const layer2 = vl
+    .markLine()
+    .encode(
+      vl.x().fieldT('date'),
+      vl.y().fieldQ('price').stack('zero')
+    );
+
+  const layer3 = vl
+    .markPoint({opacity: 1, filled: true})
+    .encode(
+      vl.x().fieldT('date'),
+      vl.y().fieldQ('price').stack('zero')
+    );
+    
+  return vl
+    .layer(layer1, layer2, layer3)
+    .data('data/stocks.csv')
+    .description(`Google's stock price over time.`)
+    .transform(vl.filter(`datum.symbol==='GOOG'`))
+    .toSpec();
+}
 
 /*
 {
--- src/area_overlay_with_y2.vl.js
+++ src/area_overlay_with_y2.vl.js
@@ -4,7 +4,19 @@ import * as vl from 'vega-lite-api';
  * Write a Node.JS function that uses the vega-lite-api library to
  * generate and return the vega-lite JSON spec below.
  */
-
+export default function chart() {
+  return vl
+    .markArea({line: true, point: true})
+    .data('data/stocks.csv')
+    .description(`Google's stock price over time.`)
+    .transform(vl.filter(`datum.symbol==='GOOG'`))
+    .encode(
+      vl.x().fieldT('date'),
+      vl.y().fieldQ('price'),
+      vl.y2().datum(0).type('quantitative')
+    )
+    .toSpec();
+}
 
 /*
 {
