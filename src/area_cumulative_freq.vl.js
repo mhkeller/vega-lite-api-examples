278a0ec24580ff63624071bb04de9ba3181ee914
Raymond Turla <raymond.turla@paynamics.net>
2023-07-10 05:09:29 +0800

    area_cumulative_freq

--- src/area_cumulative_freq.vl.js
+++ src/area_cumulative_freq.vl.js
@@ -6,6 +6,26 @@ import * as vl from 'vega-lite-api';
  */
 
 
+export default function chart() {
+  return vl
+    .markArea()
+    .data('data/movies.json')
+    .transform(
+      vl.window({
+        op: 'count',
+        field: 'count',
+        as: 'Cumulative Count'
+      })
+      .sort([{field: 'IMDB Rating'}])
+      .frame([null, 0])
+    )
+    .encode(
+      vl.x().fieldQ('IMDB Rating'),
+      vl.y().fieldQ('Cumulative Count')
+    )
+    .toSpec()
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
