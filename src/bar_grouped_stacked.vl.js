01d581edce9c0b8d81e9f58cd534aaecacfd605b
Raymond Turla <raymond.turla@paynamics.net>
2023-07-20 12:01:22 +0800

    bar_grouped_stacked

--- src/bar_grouped_stacked.vl.js
+++ src/bar_grouped_stacked.vl.js
@@ -5,6 +5,18 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+  return vl
+    .markBar()
+    .data('data/cars.json')
+    .encode(
+      vl.x().fieldN('Cylinders'),
+      vl.xOffset().fieldN('Origin'),
+      vl.y().aggregate('sum').fieldQ('Weight_in_lbs'),
+      vl.color().timeUnit('year').fieldN('Year').scale({scheme: 'category20'})
+    )
+    .toSpec();
+}
 
 /*
 {
