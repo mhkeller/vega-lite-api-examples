c90b68d117e930019408ab855869538479648894
Raymond Turla <raymond.turla@paynamics.net>
2023-07-20 22:14:21 +0800

    boxplot_minmax_2D_vertical

--- src/boxplot_minmax_2D_vertical.vl.js
+++ src/boxplot_minmax_2D_vertical.vl.js
@@ -5,6 +5,18 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+  return vl
+    .markBoxplot({extent: 'min-max'})
+    .data('data/penguins.json')
+    .description('A vertical box plot showing median, min, and max body mass of penguins.')
+    .encode(
+      vl.x().fieldN('Species'),
+      vl.y().fieldQ('Body Mass (g)').scale({zero: false}),
+      vl.color().fieldN('Species').legend(null)
+    )
+    .toSpec();
+}
 
 /*
 {
