b0096276ccbd68649f201f96e33d27f61a67a14b
Raymond Turla <raymond.turla@paynamics.net>
2023-07-20 09:47:17 +0800

    Convert mark values without using generator

--- test/area.test.js
+++ test/area.test.js
@@ -4,23 +4,25 @@ import { readFileSync } from 'fs';
 
 import fn from '../src/area.vl.js';
 
-function * iter(obj) {
-	for (let [key, value] of Object.entries(obj)) {
-			if (Object(value) !== value) yield [obj, key, value];
-			else yield * iter(value);
-	}
+function convertMarkValues(obj) {
+  for (let key in obj) {
+    if (typeof obj[key] === "object") {
+        convertMarkValues(obj[key]);
+    }
+    if (key === "mark" && typeof obj[key] === 'string') {
+      obj.mark = { type: obj[key] }
+    }
+  }
+
+	return obj
 }
 
 const name = 'area';
 
-const spec = JSON.parse(readFileSync(`./specs/${name}.vl.json`, 'utf-8'));
+let spec = JSON.parse(readFileSync(`./specs/${name}.vl.json`, 'utf-8'));
 
 // Changes mark fields that are nested in any objects from a string to an object, which is what vega-lite-api creates
-for (let [obj] of iter(spec)) {
-	if (typeof obj.mark === "string") {
-		obj.mark = { type: obj.mark } 
-	}
-}
+spec = convertMarkValues(spec)
 
 describe(`${name}.vg.js`, () => {
 	it(`should match spec`, () => {
