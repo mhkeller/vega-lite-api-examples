3120499c852f369ffc50cb141e3085872c87bbbf
Raymond Turla <raymond.turla@paynamics.net>
2023-07-20 23:55:28 +0800

    boxplot_preaggregated

--- src/boxplot_preaggregated.vl.js
+++ src/boxplot_preaggregated.vl.js
@@ -5,6 +5,74 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+  const layer1 = vl
+    .markRule()
+    .encode(
+      vl.x().fieldQ('lower').title(null).scale({zero: false}),
+      vl.x2().field('upper')
+    );
+
+  const layer2 = vl
+    .markBar({size: 14})
+    .encode(
+      vl.x().fieldQ('q1'),
+      vl.x2().field('q3'),
+      vl.color().fieldN('Species').legend(null)
+    );
+
+  const layer3 = vl
+    .markTick({color: 'white', size: 14})
+    .encode(
+      vl.x().fieldQ('median'),
+    );
+
+  const layer4 = vl
+    .markPoint({style: 'boxplot-outliers'})
+    .transform(vl.flatten('outliers'))
+    .encode(
+      vl.x().fieldQ('outliers'),
+    );
+
+  return vl
+    .layer(layer1, layer2, layer3, layer4)
+    .data({
+      values: [
+        {
+          Species: 'Adelie',
+          lower: 2850,
+          q1: 3350,
+          median: 3700,
+          q3: 4000,
+          upper: 4775,
+          outliers: []
+        } ,
+        {
+          Species: 'Chinstrap',
+          lower: 2700,
+          q1: 3487.5,
+          median: 3700,
+          q3: 3950,
+          upper: 4800,
+          outliers: [2700, 4800]
+        } ,
+        {
+          Species: 'Gentoo',
+          lower: 3950,
+          q1: 4700,
+          median: 5000,
+          q3: 5500,
+          upper: 6300,
+          outliers: []
+        }
+      ]
+    })
+    .title('Body Mass of Penguin Species (g)')
+    .encode(
+      vl.y().fieldN('Species').title(null)
+    )
+    .toSpec();
+}
 
 /*
 {
