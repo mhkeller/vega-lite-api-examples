86df89b995f16130bc5754028ace75e84eaed31b
Raymond Turla <raymond.turla@paynamics.net>
2023-07-20 12:01:22 +0800

    bar_multi_values_per_categories

--- src/bar_multi_values_per_categories.vl.js
+++ src/bar_multi_values_per_categories.vl.js
@@ -5,6 +5,26 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+  return vl
+    .markBar()
+    .background('white')
+    .data({
+      values: [
+        {a: 'A', b: 28}, {a: 'A', b: 12}, {a: 'B', b: 55},
+        {a: 'C', b: 43}, {a: 'D', b: 91}, {a: 'E', b: 81},
+        {a: 'F', b: 53}, {a: 'F', b: 7}, {a: 'G', b: 21},
+        {a: 'G', b: 19}, {a: 'H', b: 87}, {a: 'I', b: 52}
+      ]
+    })
+    .encode(
+      vl.x().fieldN('a'),
+      vl.y().fieldQ('b').stack(true),
+      vl.fill().value('steelblue'),
+      vl.stroke().value('white')
+    )
+    .toSpec();
+}
 
 /*
 {
