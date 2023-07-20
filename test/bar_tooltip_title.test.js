a585ce07eafb2d6484e01a949dcf96e62362001c
Raymond Turla <raymond.turla@paynamics.net>
2023-07-20 12:08:59 +0800

    build

--- test/bar_tooltip_title.test.js
+++ test/bar_tooltip_title.test.js
@@ -4,28 +4,25 @@ import { readFileSync } from 'fs';
 
 import fn from '../src/bar_tooltip_title.vl.js';
 
+function convertMarkValues (obj) {
+	for (const key in obj) {
+		if (typeof obj[key] === 'object') {
+			convertMarkValues(obj[key]);
+		}
+		if (key === 'mark' && typeof obj[key] === 'string') {
+			obj.mark = { type: obj[key] };
+		}
+	}
+
+	return obj;
+}
+
 const name = 'bar_tooltip_title';
 
-const spec = JSON.parse(readFileSync(`./specs/${name}.vl.json`, 'utf-8'));
+let spec = JSON.parse(readFileSync(`./specs/${name}.vl.json`, 'utf-8'));
 
-// Change this from a string to an object, which is what vega-lite-api creates
-if (typeof spec.mark === 'string') {
-	spec.mark = { type: spec.mark };
-}
-
-// Change spec.mark from a string to an object, which is what vega-lite-api creates
-if (typeof spec.spec === 'object' && typeof spec.spec?.mark === 'string') {
-	spec.spec.mark = { type: spec.spec.mark };
-}
-
-// Change layer[i].mark from a string to an object, which is what vega-lite-api creates
-if (typeof spec.layer === 'object' && spec.layer.length) {
-	spec.layer.forEach(l => {
-		if (typeof l.mark === 'string') {
-			l.mark = { type: l.mark };
-		}
-	});
-}
+// Changes mark fields that are nested in any objects from a string to an object, which is what vega-lite-api creates
+spec = convertMarkValues(spec);
 
 describe(`${name}.vg.js`, () => {
 	it(`should match spec`, () => {
