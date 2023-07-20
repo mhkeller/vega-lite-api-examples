34e892e35720326cf41e2cd6eaf519606205501e
Raymond Turla <raymond.turla@paynamics.net>
2023-07-20 12:01:22 +0800

    bar_size_fit

--- src/bar_size_fit.vl.js
+++ src/bar_size_fit.vl.js
@@ -5,6 +5,18 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+  return vl
+    .markBar()
+    .width(200)
+    .height(200)
+    .data('data/cars.json')
+    .encode(
+      vl.x().field('Origin'),
+      vl.y().aggregate('count')
+    )
+    .toSpec();
+}
 
 /*
 {
