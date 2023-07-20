fe7191642d913c8cf09a28a425347f913e2ff265
Raymond Turla <raymond.turla@paynamics.net>
2023-07-20 18:21:03 +0800

    boxplot_1D_vertical

--- src/boxplot_1D_vertical.vl.js
+++ src/boxplot_1D_vertical.vl.js
@@ -5,6 +5,16 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+  return vl
+    .markBoxplot()
+    .data('data/penguins.json')
+    .description('A vertical box plot showing median and lower and upper quartiles of the distribution of body mass of penguins.')
+    .encode(
+      vl.y().fieldQ('Body Mass (g)').scale({zero: false})
+    )
+    .toSpec();
+}
 
 /*
 {
