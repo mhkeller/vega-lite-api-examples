8a2ff8d6d84764ca20e54f0ef942c0b91224a6dc
Raymond Turla <raymond.turla@paynamics.net>
2023-07-21 10:30:34 +0800

    circle_natural_disasters

--- src/circle_natural_disasters.vl.js
+++ src/circle_natural_disasters.vl.js
@@ -5,6 +5,26 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+  return vl
+    .markCircle({
+      opacity: 0.8,
+      stroke: 'black',
+      strokeWidth: 1
+    })
+    .width(600)
+    .height(400)
+    .data('data/disasters.csv')
+    .transform(vl.filter(`datum.Entity !== 'All natural disasters'`))
+    .encode(
+      vl.x().fieldT('Year').axis({grid: false}),
+      vl.y().fieldN('Entity').axis({title: ''}),
+      vl.size().fieldQ('Deaths').title('Annual Global Deaths')
+        .legend({clipHeight: 30}).scale({rangeMax: 5000}),
+      vl.color().fieldN('Entity').legend(null)
+    )
+    .toSpec();
+}
 
 /*
 {
