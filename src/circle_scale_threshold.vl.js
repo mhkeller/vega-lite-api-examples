abe77b5bce96b7a4b18bafeea66431a17c5e0e23
Raymond Turla <raymond.turla@paynamics.net>
2023-07-21 14:48:45 +0800

    circle_scale_threshold

--- src/circle_scale_threshold.vl.js
+++ src/circle_scale_threshold.vl.js
@@ -5,6 +5,32 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+  return vl
+    .markCircle()
+    .data({
+      values: [
+        {a: 'A', b: 28},
+        {a: 'B', b: 55},
+        {a: 'C', b: 43},
+        {a: 'D', b: 91},
+        {a: 'E', b: 81},
+        {a: 'F', b: 53},
+        {a: 'G', b: 19},
+        {a: 'H', b: 87},
+        {a: 'I', b: 52}
+      ]
+    })
+    .encode(
+      vl.y().fieldN('b').axis({ticks: false, domain: false, title: null}).sort(null),
+      vl.size().fieldQ('b')
+        .scale({type: 'threshold', type: 'threshold', domain: [30, 70], range: [80, 200, 320]}),
+      vl.color().fieldQ('b')
+        .scale({type: 'threshold', domain: [30, 70], scheme: 'viridis'})
+        .legend({title: 'Threshold'})
+    )
+    .toSpec();
+}
 
 /*
 {
