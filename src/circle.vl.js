c11f56aed86c0bd6ac5f5bb69dd5bfa6996e81fb
Raymond Turla <raymond.turla@paynamics.net>
2023-07-21 15:23:50 +0800

    circle

--- src/circle.vl.js
+++ src/circle.vl.js
@@ -5,6 +5,17 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+  return vl
+  .markCircle()
+  .data('data/cars.json')
+    .encode(
+      vl.x().fieldQ('Horsepower'),
+      vl.y().fieldQ('Miles_per_Gallon'),
+    )
+    .toSpec();
+}
+
 
 /*
 {
