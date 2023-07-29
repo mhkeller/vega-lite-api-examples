6c09e39a02e2375bb20ccfc5dd602b55830ca5ec
mhkeller <code@mhkeller.com>
2023-07-29 18:23:01 -0500

    point_2d_config_no_zero

--- src/point_2d_config_no_zero.vl.js
+++ src/point_2d_config_no_zero.vl.js
@@ -4,7 +4,17 @@ import * as vl from 'vega-lite-api';
  * Write a Node.JS function that uses the vega-lite-api library to
  * generate and return the vega-lite JSON spec below.
  */
-
+export default function chart () {
+	return vl.markPoint()
+		.description('A scatterplot showing horsepower and miles per gallons for various cars.')
+		.data('data/cars.json')
+		.encode(
+			vl.x().fieldQ('Horsepower'),
+			vl.y().fieldQ('Miles_per_Gallon')
+		)
+		.config({ scale: { zero: false } })
+		.toSpec();
+}
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
