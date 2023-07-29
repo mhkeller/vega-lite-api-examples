1c1745ec812ad25740e0b110e2301661a5df4b25
mhkeller <code@mhkeller.com>
2023-07-29 18:39:52 -0500

    point_background

--- src/point_background.vl.js
+++ src/point_background.vl.js
@@ -4,7 +4,18 @@ import * as vl from 'vega-lite-api';
  * Write a Node.JS function that uses the vega-lite-api library to
  * generate and return the vega-lite JSON spec below.
  */
-
+export default function chart () {
+	return vl.markPoint()
+		.description('A scatterplot with orange overall background and yellow view background.')
+		.data('data/cars.json')
+		.background('orange')
+		.view({ fill: 'yellow' })
+		.encode(
+			vl.x().fieldQ('Horsepower'),
+			vl.y().fieldQ('Miles_per_Gallon')
+		)
+		.toSpec();
+}
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
