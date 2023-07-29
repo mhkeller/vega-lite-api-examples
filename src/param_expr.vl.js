1c7021b31999f9529c0ef62080095278d4f46ed1
mhkeller <code@mhkeller.com>
2023-07-29 18:06:17 -0500

    param_expr

--- src/param_expr.vl.js
+++ src/param_expr.vl.js
@@ -4,7 +4,34 @@ import * as vl from 'vega-lite-api';
  * Write a Node.JS function that uses the vega-lite-api library to
  * generate and return the vega-lite JSON spec below.
  */
+export default function chart () {
+	// Both of these styles are valid
+	const size = vl.expr('sel.Miles_per_Gallon * 10 || 75');
+	const opacity = { expr: 'opacityVar/100' };
 
+	return vl.markPoint({ size, opacity })
+		.data('data/cars.json')
+		.params([
+			{
+				name: 'opacityVar',
+				value: 50,
+				bind: { input: 'range', min: 1, max: 100 }
+			},
+			{
+				name: 'sel',
+				select: {
+					type: 'point',
+					fields: ['Miles_per_Gallon'],
+					toggle: false
+				}
+			}
+		])
+		.encode(
+			vl.x().fieldQ('Horsepower'),
+			vl.y().fieldQ('Miles_per_Gallon')
+		)
+		.toSpec();
+}
 
 /*
 {
