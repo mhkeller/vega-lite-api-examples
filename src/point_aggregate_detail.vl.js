284d0a72aca25905a3b0469c460133c97c008a2b
mhkeller <code@mhkeller.com>
2023-07-29 18:27:46 -0500

    point_aggregate_detail

--- src/point_aggregate_detail.vl.js
+++ src/point_aggregate_detail.vl.js
@@ -4,7 +4,17 @@ import * as vl from 'vega-lite-api';
  * Write a Node.JS function that uses the vega-lite-api library to
  * generate and return the vega-lite JSON spec below.
  */
-
+export default function chart () {
+	return vl.markPoint()
+		.description('A scatterplot showing average horsepower and displacement for cars from different origins.')
+		.data('data/cars.json')
+		.encode(
+			vl.x().aggregate('mean').field('Horsepower'),
+			vl.y().aggregate('mean').field('Displacement'),
+			vl.detail().field('Origin')
+		)
+		.toSpec();
+}
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
