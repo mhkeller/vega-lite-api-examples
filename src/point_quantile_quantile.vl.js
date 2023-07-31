e46ecb77c1d58e77e7eeaa7608c620bc67b235ea
mhkeller <code@mhkeller.com>
2023-07-31 19:20:39 -0400

    point_quantile_quantile

--- src/point_quantile_quantile.vl.js
+++ src/point_quantile_quantile.vl.js
@@ -4,7 +4,24 @@ import * as vl from 'vega-lite-api';
  * Write a Node.JS function that uses the vega-lite-api library to
  * generate and return the vega-lite JSON spec below.
  */
-
+export default function chart () {
+	return vl
+		.data('data/normal-2d.json')
+		.transform(
+			vl.quantile('u').step(0.01).as(['p', 'v']),
+			vl.calculate('quantileUniform(datum.p)').as('unif'),
+			vl.calculate('quantileNormal(datum.p)').as('norm')
+		)
+		.hconcat(
+			vl
+				.markPoint()
+				.encode(vl.x().fieldQ('unif'), vl.y().fieldQ('v')),
+			vl
+				.markPoint()
+				.encode(vl.x().fieldQ('norm'), vl.y().fieldQ('v'))
+		)
+		.toSpec();
+}
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
