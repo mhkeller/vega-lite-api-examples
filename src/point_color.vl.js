91c132d27c4ee49ccc7564e3fc40bea7e1cccd6c
mhkeller <code@mhkeller.com>
2023-08-01 21:14:14 -0400

    point_invalid_color

--- src/point_color.vl.js
+++ src/point_color.vl.js
@@ -4,7 +4,7 @@ import * as vl from 'vega-lite-api';
  * Write a Node.JS function that uses the vega-lite-api library to
  * generate and return the vega-lite JSON spec below.
  */
-export default function chart() {
+export default function chart () {
 	return vl
 		.markPoint()
 		.data('data/cars.json')
--- src/point_invalid_color.vl.js
+++ src/point_invalid_color.vl.js
@@ -4,7 +4,23 @@ import * as vl from 'vega-lite-api';
  * Write a Node.JS function that uses the vega-lite-api library to
  * generate and return the vega-lite JSON spec below.
  */
-
+export default function chart () {
+	return vl
+		.markPoint()
+		.data('data/movies.json')
+		.encode(
+			vl.x().fieldQ('IMDB Rating'),
+			vl.y().fieldQ('Rotten Tomatoes Rating'),
+			vl.color().condition({
+				test: 'datum[\'IMDB Rating\'] === null || datum[\'Rotten Tomatoes Rating\'] === null',
+				value: '#aaa'
+			})
+		)
+		.config({
+			mark: { invalid: null }
+		})
+		.toSpec();
+}
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
