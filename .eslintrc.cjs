bccf92f722e03a6d04e1f35402af67a2a4126fcd
mhkeller <code@mhkeller.com>
2025-05-14 23:31:33 -0400

    remove eslint

--- .eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	extends: ['semistandard'],
-	rules: {
-		'arrow-parens': ['error', 'as-needed'],
-		'no-tabs': ['error', { allowIndentationTabs: true }],
-		quotes: ['error', 'single', { allowTemplateLiterals: true }],
-		indent: ['error', 'tab']
-	}
-};
