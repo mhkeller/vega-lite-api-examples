e159948c88cf8f282fda655ef6a2d3e76be32ac4
mhkeller <code@mhkeller.com>
2026-01-25 22:38:12 -0500

    add trellis_barley

--- src/trellis_barley.vl.js
+++ src/trellis_barley.vl.js
@@ -5,6 +5,32 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.markPoint()
+		.name('trellis_barley')
+		.description(
+			'The Trellis display by Becker et al. helped establish small multiples as a \u201Cpowerful mechanism for understanding interactions in studies of how a response depends on explanatory variables\u201D. Here we reproduce a trellis of Barley yields from the 1930s, complete with main-effects ordering to facilitate comparison.'
+		)
+		.data('data/barley.json')
+		.height({ step: 12 })
+		.encode(
+			vl
+				.facet()
+				.fieldO('site')
+				.columns(2)
+				.sort({ op: 'median', field: 'yield' }),
+			vl
+				.x()
+				.aggregate('median')
+				.fieldQ('yield')
+				.scale({ zero: false }),
+			vl.y().fieldO('variety').sort('-x'),
+			vl.color().fieldN('year')
+		)
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
