a69b4b96c10ad2eadc69f6d4300930a3e78aa587
mhkeller <code@mhkeller.com>
2026-01-26 17:46:03 -0500

    add nested_concat_align

--- src/nested_concat_align.vl.js
+++ src/nested_concat_align.vl.js
@@ -5,6 +5,43 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		description: 'Nested concatenation aligned by setting axis minExtent',
+		data: { url: 'data/movies.json' },
+		vconcat: [
+			{
+				title: 'Ratings',
+				repeat: { column: ['Rotten Tomatoes Rating', 'IMDB Rating'] },
+				spec: {
+					width: 150,
+					height: 50,
+					mark: { type: 'bar' },
+					encoding: {
+						x: { field: { repeat: 'column' }, bin: { maxbins: 20 } },
+						y: { aggregate: 'count' }
+					}
+				}
+			},
+			{
+				title: 'Gross',
+				repeat: { column: ['US Gross', 'Worldwide Gross'] },
+				spec: {
+					width: 150,
+					height: 50,
+					encoding: {
+						x: { field: { repeat: 'column' }, bin: { maxbins: 20 } },
+						y: { aggregate: 'count' }
+					},
+					mark: { type: 'bar' }
+				}
+			}
+		],
+		config: { countTitle: 'Count', axisX: { titleLimit: 150 }, axisY: { minExtent: 40 } }
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
