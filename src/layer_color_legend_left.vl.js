5c1838bc52ffecfad7fcf31fbb97973f4f72e54e
mhkeller <code@mhkeller.com>
2026-01-26 09:42:22 -0500

    add layer_color_legend_left

--- src/layer_color_legend_left.vl.js
+++ src/layer_color_legend_left.vl.js
@@ -5,6 +5,27 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		description: 'Stock price over time with color legend orient = left.',
+		data: { url: 'data/stocks.csv' },
+		encoding: {
+			x: { field: 'date', type: 'temporal' },
+			y: { field: 'price', type: 'quantitative' },
+			color: {
+				field: 'symbol',
+				type: 'nominal',
+				legend: { orient: 'left' }
+			}
+		},
+		layer: [
+			{ mark: { type: 'line' } },
+			{ mark: { type: 'point', filled: true } }
+		]
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
