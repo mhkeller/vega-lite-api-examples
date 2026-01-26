e625095d713b7f21a00fd1425ffa3fd9f3b9e7b0
mhkeller <code@mhkeller.com>
2026-01-25 22:31:54 -0500

    add geo_text

--- src/geo_text.vl.js
+++ src/geo_text.vl.js
@@ -5,6 +5,35 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	const spec = vl
+		.layer(
+			vl
+				.markGeoshape({ fill: 'lightgray', stroke: 'white' })
+				.data({
+					url: 'data/us-10m.json',
+					format: { type: 'topojson', feature: 'states' }
+				}),
+			vl
+				.layer(
+					vl.markCircle({ color: 'orange' }),
+					vl
+						.markText({ dy: -10 })
+						.encode(vl.text().fieldN('city'))
+				)
+				.data('data/us-state-capitals.json')
+				.encode(
+					vl.longitude().fieldQ('lon'),
+					vl.latitude().fieldQ('lat')
+				)
+		)
+		.width(800)
+		.height(500)
+		.toSpec();
+	spec.projection = { type: 'albersUsa' };
+	return spec;
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
