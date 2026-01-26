f20c9dd428cd374d64f67ec6a63b957c408d9588
mhkeller <code@mhkeller.com>
2026-01-25 21:38:42 -0500

    add tick_sort

--- src/tick_sort.vl.js
+++ src/tick_sort.vl.js
@@ -5,6 +5,14 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.markTick()
+		.data('data/cars.json')
+		.encode(vl.x().fieldQ('Horsepower').sort('descending'))
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
