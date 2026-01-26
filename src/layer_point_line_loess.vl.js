163f15ce307cf9d8923622d1cb3e04451afd7652
mhkeller <code@mhkeller.com>
2026-01-25 22:29:50 -0500

    add layer_point_line_loess

--- src/layer_point_line_loess.vl.js
+++ src/layer_point_line_loess.vl.js
@@ -5,6 +5,28 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.layer(
+			vl.markPoint({ filled: true }).encode(
+				vl.x().fieldQ('Rotten Tomatoes Rating'),
+				vl.y().fieldQ('IMDB Rating')
+			),
+			vl
+				.markLine({ color: 'firebrick' })
+				.transform({
+					loess: 'IMDB Rating',
+					on: 'Rotten Tomatoes Rating'
+				})
+				.encode(
+					vl.x().fieldQ('Rotten Tomatoes Rating'),
+					vl.y().fieldQ('IMDB Rating')
+				)
+		)
+		.data('data/movies.json')
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
