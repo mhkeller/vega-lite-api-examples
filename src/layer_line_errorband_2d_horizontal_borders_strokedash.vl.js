3657dc672100619544232d023d39e04ab1e6fd54
mhkeller <code@mhkeller.com>
2026-01-26 09:44:31 -0500

    add layer_line_errorband_2d_horizontal_borders_strokedash

--- src/layer_line_errorband_2d_horizontal_borders_strokedash.vl.js
+++ src/layer_line_errorband_2d_horizontal_borders_strokedash.vl.js
@@ -5,6 +5,35 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: { url: 'data/cars.json' },
+		encoding: {
+			x: { field: 'Year', timeUnit: 'year' },
+			y: {
+				type: 'quantitative',
+				scale: { zero: false },
+				axis: { title: 'Miles per Gallon (95% CIs)' }
+			}
+		},
+		layer: [
+			{
+				mark: {
+					type: 'errorband',
+					extent: 'ci',
+					borders: { opacity: 0.5, strokeDash: [6, 4] }
+				},
+				encoding: { y: { field: 'Miles_per_Gallon' } }
+			},
+			{
+				mark: { type: 'line' },
+				encoding: { y: { field: 'Miles_per_Gallon', aggregate: 'mean' } }
+			}
+		]
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
