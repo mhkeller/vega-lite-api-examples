2112a31c417d3f51ef2a1faf0d243a398e096d6f
mhkeller <code@mhkeller.com>
2026-01-26 08:52:02 -0500

    add errorbar_horizontal_aggregate

--- src/errorbar_horizontal_aggregate.vl.js
+++ src/errorbar_horizontal_aggregate.vl.js
@@ -5,6 +5,38 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.layer(
+			vl.markRule().encode(
+				vl.y().fieldO('age'),
+				vl.x().aggregate('min').fieldQ('people').title('population'),
+				vl.x2().aggregate('max').field('people')
+			),
+			vl.markTick().encode(
+				vl.y().fieldO('age'),
+				vl.x().aggregate('min').fieldQ('people').title('population'),
+				vl.size().value(5)
+			),
+			vl.markTick().encode(
+				vl.y().fieldO('age'),
+				vl.x().aggregate('max').fieldQ('people').title('population'),
+				vl.size().value(5)
+			),
+			vl.markPoint().encode(
+				vl.y().fieldO('age'),
+				vl.x().aggregate('mean').fieldQ('people').title('population'),
+				vl.size().value(2)
+			)
+		)
+		.description(
+			'A bar chart showing the US population distribution of age groups in 2000.'
+		)
+		.data('data/population.json')
+		.transform({ filter: 'datum.year == 2000' })
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
