955f10002aa79dff0edf44b3c1ccba0a9000e854
mhkeller <code@mhkeller.com>
2026-01-26 09:07:14 -0500

    add interactive_multi_line_tooltip

--- src/interactive_multi_line_tooltip.vl.js
+++ src/interactive_multi_line_tooltip.vl.js
@@ -5,6 +5,51 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: { url: 'data/seattle-weather.csv' },
+		encoding: {
+			x: { timeUnit: 'yearmonthdate', field: 'date' },
+			tooltip: [
+				{ timeUnit: 'yearmonthdate', field: 'date' },
+				{ field: 'temp_max', type: 'quantitative' },
+				{ field: 'temp_min', type: 'quantitative' }
+			]
+		},
+		layer: [
+			{
+				mark: { type: 'line', color: 'orange' },
+				encoding: { y: { field: 'temp_max', type: 'quantitative' } }
+			},
+			{
+				mark: { type: 'line', color: 'red' },
+				encoding: { y: { field: 'temp_min', type: 'quantitative' } }
+			},
+			{
+				mark: { type: 'rule' },
+				params: [
+					{
+						name: 'hover',
+						select: { type: 'point', on: 'mouseover' }
+					}
+				],
+				encoding: {
+					color: {
+						condition: {
+							param: 'hover',
+							empty: false,
+							value: 'black'
+						},
+						value: 'transparent'
+					}
+				}
+			}
+		],
+		config: { axisY: { minExtent: 30 } }
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
