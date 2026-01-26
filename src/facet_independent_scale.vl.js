0ff6b0c39876360a2354fd3748342909413dcf8e
mhkeller <code@mhkeller.com>
2026-01-25 22:36:08 -0500

    add facet_independent_scale

--- src/facet_independent_scale.vl.js
+++ src/facet_independent_scale.vl.js
@@ -5,6 +5,34 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		description:
+			'A trellis bar chart showing the US population distribution of age groups and gender in 2000.',
+		data: { url: 'data/population.json' },
+		transform: [
+			{ filter: 'datum.year == 2000' },
+			{ calculate: "datum.sex == 2 ? 'Female' : 'Male'", as: 'gender' }
+		],
+		facet: { column: { field: 'gender' } },
+		spec: {
+			width: { step: 17 },
+			mark: { type: 'bar' },
+			encoding: {
+				y: { aggregate: 'sum', field: 'people', title: 'population' },
+				x: { field: 'age', type: 'ordinal' },
+				color: {
+					field: 'gender',
+					scale: { range: ['#675193', '#ca8861'] }
+				}
+			}
+		},
+		resolve: { scale: { x: 'independent' } },
+		config: { view: { fill: 'yellow' } }
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
