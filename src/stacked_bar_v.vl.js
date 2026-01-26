146a94c9bfef750e403593f7ffaceee90c645a20
mhkeller <code@mhkeller.com>
2026-01-25 21:49:27 -0500

    add stacked_bar_v

--- src/stacked_bar_v.vl.js
+++ src/stacked_bar_v.vl.js
@@ -5,6 +5,18 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.markBar()
+		.data('data/barley.json')
+		.encode(
+			vl.x().field('variety'),
+			vl.y().aggregate('sum').field('yield'),
+			vl.color().field('site')
+		)
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
