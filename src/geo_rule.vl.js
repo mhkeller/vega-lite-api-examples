d82236cbfcee56ae676e3ba2157dd17ca03528d7
mhkeller <code@mhkeller.com>
2026-01-26 08:57:38 -0500

    add geo_rule

--- src/geo_rule.vl.js
+++ src/geo_rule.vl.js
@@ -5,6 +5,67 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		width: 800,
+		height: 500,
+		layer: [
+			{
+				data: {
+					url: 'data/us-10m.json',
+					format: { type: 'topojson', feature: 'states' }
+				},
+				projection: { type: 'albersUsa' },
+				mark: { type: 'geoshape', fill: 'lightgray', stroke: 'white' }
+			},
+			{
+				data: { url: 'data/airports.csv' },
+				projection: { type: 'albersUsa' },
+				mark: { type: 'circle' },
+				encoding: {
+					longitude: { field: 'longitude', type: 'quantitative' },
+					latitude: { field: 'latitude', type: 'quantitative' },
+					size: { value: 5 },
+					color: { value: 'gray' }
+				}
+			},
+			{
+				data: { url: 'data/flights-airport.csv' },
+				transform: [
+					{ filter: { field: 'origin', equal: 'SEA' } },
+					{
+						lookup: 'origin',
+						from: {
+							data: { url: 'data/airports.csv' },
+							key: 'iata',
+							fields: ['latitude', 'longitude']
+						},
+						as: ['origin_latitude', 'origin_longitude']
+					},
+					{
+						lookup: 'destination',
+						from: {
+							data: { url: 'data/airports.csv' },
+							key: 'iata',
+							fields: ['latitude', 'longitude']
+						},
+						as: ['dest_latitude', 'dest_longitude']
+					}
+				],
+				projection: { type: 'albersUsa' },
+				mark: { type: 'rule' },
+				encoding: {
+					longitude: { field: 'origin_longitude', type: 'quantitative' },
+					latitude: { field: 'origin_latitude', type: 'quantitative' },
+					longitude2: { field: 'dest_longitude' },
+					latitude2: { field: 'dest_latitude' }
+				}
+			}
+		]
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
