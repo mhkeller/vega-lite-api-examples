75b420211147913b161a29cc05d0d2e89a4a8012
mhkeller <code@mhkeller.com>
2026-01-26 15:54:45 -0500

    add line_overlay_stroked

--- src/line_overlay_stroked.vl.js
+++ src/line_overlay_stroked.vl.js
@@ -5,6 +5,20 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		description: 'Stock prices of 5 Tech Companies over Time.',
+		data: { url: 'data/stocks.csv' },
+		mark: { type: 'line', point: { filled: false, fill: 'white' } },
+		encoding: {
+			x: { timeUnit: 'year', field: 'date' },
+			y: { aggregate: 'mean', field: 'price', type: 'quantitative' },
+			color: { field: 'symbol', type: 'nominal' }
+		}
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
