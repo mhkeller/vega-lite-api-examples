57946c89711b0f3a6145cddfe46a51a6734fd9c4
mhkeller <code@mhkeller.com>
2026-01-25 21:51:58 -0500

    add sequence_line

--- src/sequence_line.vl.js
+++ src/sequence_line.vl.js
@@ -5,6 +5,20 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.markLine()
+		.description('Plots a function using a generated sequence.')
+		.width(300)
+		.height(150)
+		.data({
+			sequence: { start: 0, stop: 12.7, step: 0.1, as: 'x' }
+		})
+		.transform({ calculate: 'sin(datum.x)', as: 'sin(x)' })
+		.encode(vl.x().fieldQ('x'), vl.y().fieldQ('sin(x)'))
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
