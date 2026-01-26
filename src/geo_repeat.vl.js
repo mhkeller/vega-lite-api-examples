6b69b632742c4bf0ca3a4b54e5b306e30ada7aae
mhkeller <code@mhkeller.com>
2026-01-26 08:58:07 -0500

    add geo_repeat

--- src/geo_repeat.vl.js
+++ src/geo_repeat.vl.js
@@ -5,6 +5,40 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		description:
+			'the population per state, engineers per state, and hurricanes per state',
+		repeat: { row: ['population', 'engineers', 'hurricanes'] },
+		resolve: { scale: { color: 'independent' } },
+		spec: {
+			width: 500,
+			height: 300,
+			data: { url: 'data/population_engineers_hurricanes.csv' },
+			transform: [
+				{
+					lookup: 'id',
+					from: {
+						data: {
+							url: 'data/us-10m.json',
+							format: { type: 'topojson', feature: 'states' }
+						},
+						key: 'id'
+					},
+					as: 'geo'
+				}
+			],
+			projection: { type: 'albersUsa' },
+			mark: { type: 'geoshape' },
+			encoding: {
+				shape: { field: 'geo', type: 'geojson' },
+				color: { field: { repeat: 'row' }, type: 'quantitative' }
+			}
+		}
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
