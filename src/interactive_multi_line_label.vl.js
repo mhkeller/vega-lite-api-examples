1fb9cc44e6f3929b810133dc0aed4ff4b5aadf9f
mhkeller <code@mhkeller.com>
2026-01-26 09:07:14 -0500

    add interactive_multi_line_label

--- src/interactive_multi_line_label.vl.js
+++ src/interactive_multi_line_label.vl.js
@@ -5,6 +5,87 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: { url: 'data/stocks.csv' },
+		width: 400,
+		height: 300,
+		layer: [
+			{
+				encoding: {
+					x: { field: 'date', type: 'temporal' },
+					y: { field: 'price', type: 'quantitative' },
+					color: { field: 'symbol', type: 'nominal' }
+				},
+				layer: [
+					{ mark: { type: 'line' } },
+					{
+						params: [
+							{
+								name: 'label',
+								select: {
+									type: 'point',
+									encodings: ['x'],
+									nearest: true,
+									on: 'mouseover'
+								}
+							}
+						],
+						mark: { type: 'point' },
+						encoding: {
+							opacity: {
+								condition: {
+									param: 'label',
+									empty: false,
+									value: 1
+								},
+								value: 0
+							}
+						}
+					}
+				]
+			},
+			{
+				transform: [{ filter: { param: 'label', empty: false } }],
+				layer: [
+					{
+						mark: { type: 'rule', color: 'gray' },
+						encoding: {
+							x: { type: 'temporal', field: 'date', aggregate: 'min' }
+						}
+					},
+					{
+						encoding: {
+							text: { type: 'quantitative', field: 'price' },
+							x: { type: 'temporal', field: 'date' },
+							y: { type: 'quantitative', field: 'price' }
+						},
+						layer: [
+							{
+								mark: {
+									type: 'text',
+									stroke: 'white',
+									strokeWidth: 2,
+									align: 'left',
+									dx: 5,
+									dy: -5
+								}
+							},
+							{
+								mark: { type: 'text', align: 'left', dx: 5, dy: -5 },
+								encoding: {
+									color: { type: 'nominal', field: 'symbol' }
+								}
+							}
+						]
+					}
+				]
+			}
+		]
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
