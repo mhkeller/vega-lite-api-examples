055f6f0bd7fd7c0c811595e558052db418e8d7cb
mhkeller <code@mhkeller.com>
2026-01-25 21:48:30 -0500

    add sample_scatterplot

--- src/sample_scatterplot.vl.js
+++ src/sample_scatterplot.vl.js
@@ -5,6 +5,25 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.hconcat(
+			vl.markPoint().encode(
+				vl.x().fieldQ('Horsepower'),
+				vl.y().fieldQ('Miles_per_Gallon')
+			),
+			vl
+				.markPoint()
+				.transform({ sample: 200 })
+				.encode(
+					vl.x().fieldQ('Horsepower'),
+					vl.y().fieldQ('Miles_per_Gallon')
+				)
+		)
+		.data('data/cars.json')
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
