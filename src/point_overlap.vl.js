68fd63610fa2c75061ee0684767adfc017a46ed7
mhkeller <code@mhkeller.com>
2026-01-25 21:53:12 -0500

    add point_overlap

--- src/point_overlap.vl.js
+++ src/point_overlap.vl.js
@@ -4,17 +4,28 @@ import * as vl from 'vega-lite-api';
  * Write a Node.JS function that uses the vega-lite-api library to
  * generate and return the vega-lite JSON spec below.
  */
-// export default function chart () {
-// return vl
-// 	.markPoint()
-// 	.data([
-// 		{ a: 'C', b: 2 }, { a: 'C', b: 7 }, { a: 'C', b: 4 },
-// 		{ a: 'D', b: 1 }, { a: 'D', b: 2 }, { a: 'D', b: 6 },
-// 		{ a: 'E', b: 8 }, { a: 'E', b: 4 }, { a: 'E', b: 7 }
-// 	])
-// 	.encode()
-// 	.toSpec();
-// }
+
+export default function chart() {
+	const spec = vl
+		.markPoint()
+		.data({
+			values: [
+				{ a: 'C', b: 2 },
+				{ a: 'C', b: 7 },
+				{ a: 'C', b: 4 },
+				{ a: 'D', b: 1 },
+				{ a: 'D', b: 2 },
+				{ a: 'D', b: 6 },
+				{ a: 'E', b: 8 },
+				{ a: 'E', b: 4 },
+				{ a: 'E', b: 7 }
+			]
+		})
+		.toSpec();
+	spec.encoding = {};
+	return spec;
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
