00089016b987f44a13477464e0a23cc013bdc41c
mhkeller <code@mhkeller.com>
2026-01-25 22:14:03 -0500

    add line_inside_domain_using_clip

--- src/line_inside_domain_using_clip.vl.js
+++ src/line_inside_domain_using_clip.vl.js
@@ -5,6 +5,25 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.markLine({ clip: true })
+		.data({
+			values: [
+				{ wavelength: 250, power: 1 },
+				{ wavelength: 300, power: 2 },
+				{ wavelength: 420, power: 4 },
+				{ wavelength: 450, power: 1.8 },
+				{ wavelength: 500, power: 1.1 }
+			]
+		})
+		.encode(
+			vl.x().fieldQ('wavelength').scale({ domain: [300, 450] }),
+			vl.y().fieldQ('power')
+		)
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
