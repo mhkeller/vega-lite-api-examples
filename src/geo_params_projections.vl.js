0b97e165c2aebd2965ecc581decb8361d87346b1
mhkeller <code@mhkeller.com>
2026-01-25 22:32:14 -0500

    add geo_params_projections

--- src/geo_params_projections.vl.js
+++ src/geo_params_projections.vl.js
@@ -5,6 +5,46 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	const spec = vl
+		.markGeoshape({ fill: 'lightgray', stroke: 'gray' })
+		.width(500)
+		.height(300)
+		.params([
+			{
+				name: 'projection',
+				value: 'equalEarth',
+				bind: {
+					input: 'select',
+					options: [
+						'albers',
+						'albersUsa',
+						'azimuthalEqualArea',
+						'azimuthalEquidistant',
+						'conicConformal',
+						'conicEqualArea',
+						'conicEquidistant',
+						'equalEarth',
+						'equirectangular',
+						'gnomonic',
+						'mercator',
+						'naturalEarth1',
+						'orthographic',
+						'stereographic',
+						'transverseMercator'
+					]
+				}
+			}
+		])
+		.data({
+			url: 'data/world-110m.json',
+			format: { type: 'topojson', feature: 'countries' }
+		})
+		.toSpec();
+	spec.projection = { type: { expr: 'projection' } };
+	return spec;
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
