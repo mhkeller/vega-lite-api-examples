3956a6fca97e8c95ab0b5ca71b4cdf16df380027
mhkeller <code@mhkeller.com>
2026-01-25 22:17:02 -0500

    add joinaggregate_percent_of_total

--- src/joinaggregate_percent_of_total.vl.js
+++ src/joinaggregate_percent_of_total.vl.js
@@ -5,6 +5,38 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.markBar()
+		.description(
+			'A bar graph showing what activites consume what percentage of the day.'
+		)
+		.data({
+			values: [
+				{ Activity: 'Sleeping', Time: 8 },
+				{ Activity: 'Eating', Time: 2 },
+				{ Activity: 'TV', Time: 4 },
+				{ Activity: 'Work', Time: 8 },
+				{ Activity: 'Exercise', Time: 2 }
+			]
+		})
+		.height({ step: 12 })
+		.transform(
+			{
+				joinaggregate: [{ op: 'sum', field: 'Time', as: 'TotalTime' }]
+			},
+			{
+				calculate: 'datum.Time/datum.TotalTime * 100',
+				as: 'PercentOfTotal'
+			}
+		)
+		.encode(
+			vl.x().fieldQ('PercentOfTotal').title('% of total Time'),
+			vl.y().fieldN('Activity')
+		)
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
