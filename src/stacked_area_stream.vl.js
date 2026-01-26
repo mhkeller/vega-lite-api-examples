bf3de81e655de15a4824b084fa3689215c342336
mhkeller <code@mhkeller.com>
2026-01-25 21:50:20 -0500

    add stacked_area_stream

--- src/stacked_area_stream.vl.js
+++ src/stacked_area_stream.vl.js
@@ -5,6 +5,29 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.markArea()
+		.data('data/unemployment-across-industries.json')
+		.width(300)
+		.height(200)
+		.encode(
+			vl
+				.x()
+				.timeUnit('yearmonth')
+				.field('date')
+				.axis({ domain: false, format: '%Y', tickSize: 0 }),
+			vl
+				.y()
+				.aggregate('sum')
+				.field('count')
+				.axis(null)
+				.stack('center'),
+			vl.color().field('series').scale({ scheme: 'category20b' })
+		)
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
