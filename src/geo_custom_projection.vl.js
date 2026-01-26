3509b94f1633f5be838c83dbdd23a8a548df8d37
mhkeller <code@mhkeller.com>
2026-01-26 08:58:07 -0500

    add geo_custom_projection

--- src/geo_custom_projection.vl.js
+++ src/geo_custom_projection.vl.js
@@ -5,6 +5,34 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		width: 900,
+		height: 560,
+		projection: { type: 'albersUsa', scale: 3000, translate: [1200, 700] },
+		layer: [
+			{
+				data: {
+					url: 'data/us-10m.json',
+					format: { type: 'topojson', feature: 'states' }
+				},
+				mark: { type: 'geoshape', fill: '#ccc', stroke: '#fff' }
+			},
+			{
+				data: { url: 'data/airports.csv' },
+				mark: { type: 'circle' },
+				encoding: {
+					longitude: { field: 'longitude', type: 'quantitative' },
+					latitude: { field: 'latitude', type: 'quantitative' },
+					size: { value: 10 }
+				}
+			}
+		],
+		config: { view: { stroke: null } }
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
