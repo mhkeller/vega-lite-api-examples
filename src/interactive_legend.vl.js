a85aab54f80a1585bd4b028e9be2a357908d897c
mhkeller <code@mhkeller.com>
2026-01-26 09:03:57 -0500

    add interactive_legend

--- src/interactive_legend.vl.js
+++ src/interactive_legend.vl.js
@@ -5,6 +5,41 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		width: 300,
+		height: 200,
+		data: { url: 'data/unemployment-across-industries.json' },
+		mark: { type: 'area' },
+		params: [
+			{
+				name: 'industry',
+				select: { type: 'point', fields: ['series'] },
+				bind: 'legend'
+			}
+		],
+		encoding: {
+			x: {
+				timeUnit: 'yearmonth',
+				field: 'date',
+				axis: { domain: false, format: '%Y', tickSize: 0 }
+			},
+			y: {
+				aggregate: 'sum',
+				field: 'count',
+				stack: 'center',
+				axis: null
+			},
+			color: { field: 'series', scale: { scheme: 'category20b' } },
+			opacity: {
+				condition: { param: 'industry', value: 1 },
+				value: 0.2
+			}
+		}
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
