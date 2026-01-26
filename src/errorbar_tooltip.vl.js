681b16505a4b15c51d526ebc10e5c621e893f309
mhkeller <code@mhkeller.com>
2026-01-26 08:51:00 -0500

    add errorbar_tooltip

--- src/errorbar_tooltip.vl.js
+++ src/errorbar_tooltip.vl.js
@@ -5,6 +5,18 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.markErrorbar()
+		.data('data/barley.json')
+		.encode(
+			vl.x().fieldQ('yield').scale({ zero: false }),
+			vl.y().fieldO('variety'),
+			vl.tooltip().fieldO('variety')
+		)
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
