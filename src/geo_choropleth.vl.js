b6f8dd52aaf12eb9c7d68dd23598f581a9dcb275
mhkeller <code@mhkeller.com>
2026-01-25 22:32:49 -0500

    add geo_choropleth

--- src/geo_choropleth.vl.js
+++ src/geo_choropleth.vl.js
@@ -5,6 +5,24 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	const spec = vl
+		.markGeoshape()
+		.width(500)
+		.height(300)
+		.data({
+			url: 'data/us-10m.json',
+			format: { type: 'topojson', feature: 'counties' }
+		})
+		.transform(
+			vl.lookup('id').from(vl.data('data/unemployment.tsv').key('id').fields(['rate']))
+		)
+		.encode(vl.color().fieldQ('rate'))
+		.toSpec();
+	spec.projection = { type: 'albersUsa' };
+	return spec;
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
