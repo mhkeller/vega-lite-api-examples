ad03596cece9e6bac7aaf7478534a8895d6c85b4
mhkeller <code@mhkeller.com>
2026-01-26 09:40:13 -0500

    add layer_boxplot_circle

--- src/layer_boxplot_circle.vl.js
+++ src/layer_boxplot_circle.vl.js
@@ -5,6 +5,34 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		width: 500,
+		description:
+			'A horizontal box plot showing median, min, and max in the US population distribution of age groups in 2000 and all raw data on the top.',
+		data: { url: 'data/population.json' },
+		layer: [
+			{
+				mark: { type: 'boxplot', outliers: false },
+				encoding: {
+					y: { field: 'age', type: 'ordinal' },
+					x: { field: 'people', type: 'quantitative', title: 'population' }
+				}
+			},
+			{
+				mark: { type: 'circle' },
+				encoding: {
+					y: { field: 'age', type: 'ordinal' },
+					x: { field: 'people', type: 'quantitative', title: 'population' },
+					color: { value: 'black' },
+					opacity: { value: 0.2 }
+				}
+			}
+		]
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
