1b0764737c8f25737374555e72dfbfcff10d63af
mhkeller <code@mhkeller.com>
2026-01-26 09:06:01 -0500

    add interactive_query_widgets

--- src/interactive_query_widgets.vl.js
+++ src/interactive_query_widgets.vl.js
@@ -5,6 +5,61 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		description: 'Drag the sliders to highlight points.',
+		data: { url: 'data/cars.json' },
+		transform: [{ calculate: 'year(datum.Year)', as: 'Year' }],
+		layer: [
+			{
+				params: [
+					{
+						name: 'CylYr',
+						value: [{ Cylinders: 4, Year: 1977 }],
+						select: {
+							type: 'point',
+							fields: ['Cylinders', 'Year']
+						},
+						bind: {
+							Cylinders: {
+								input: 'range',
+								min: 3,
+								max: 8,
+								step: 1
+							},
+							Year: { input: 'range', min: 1969, max: 1981, step: 1 }
+						}
+					}
+				],
+				mark: { type: 'circle' },
+				encoding: {
+					x: { field: 'Horsepower', type: 'quantitative' },
+					y: { field: 'Miles_per_Gallon', type: 'quantitative' },
+					color: {
+						condition: {
+							param: 'CylYr',
+							field: 'Origin',
+							type: 'nominal'
+						},
+						value: 'grey'
+					}
+				}
+			},
+			{
+				transform: [{ filter: { param: 'CylYr' } }],
+				mark: { type: 'circle' },
+				encoding: {
+					x: { field: 'Horsepower', type: 'quantitative' },
+					y: { field: 'Miles_per_Gallon', type: 'quantitative' },
+					color: { field: 'Origin', type: 'nominal' },
+					size: { value: 100 }
+				}
+			}
+		]
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
