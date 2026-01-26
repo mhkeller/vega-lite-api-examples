30f0224e331a7b2251da2f7664a3a574cd392396
mhkeller <code@mhkeller.com>
2026-01-26 09:05:25 -0500

    add interactive_line_brush_cursor

--- src/interactive_line_brush_cursor.vl.js
+++ src/interactive_line_brush_cursor.vl.js
@@ -5,6 +5,33 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		description:
+			'A line chart with text cursor to indicate the brush selection. The cursor changes to pointer on the mark as it can be moved with drag&drop.',
+		data: { url: 'data/stocks.csv' },
+		transform: [{ filter: "datum.symbol==='GOOG'" }],
+		mark: { type: 'line' },
+		width: 400,
+		encoding: {
+			x: { field: 'date', type: 'temporal' },
+			y: { field: 'price', type: 'quantitative' }
+		},
+		params: [
+			{
+				name: 'brush',
+				select: {
+					type: 'interval',
+					encodings: ['x'],
+					mark: { cursor: 'pointer' }
+				}
+			}
+		],
+		view: { cursor: 'text' }
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
