67889272670417f8fabc100a65de57e958e4c8b0
mhkeller <code@mhkeller.com>
2026-01-26 09:43:02 -0500

    add layer_histogram

--- src/layer_histogram.vl.js
+++ src/layer_histogram.vl.js
@@ -5,6 +5,31 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: { url: 'data/flights-2k.json' },
+		layer: [
+			{
+				mark: { type: 'bar' },
+				encoding: {
+					x: { field: 'distance', bin: true },
+					y: { aggregate: 'count' }
+				}
+			},
+			{
+				transform: [{ filter: 'datum.delay < 5' }],
+				mark: { type: 'bar' },
+				encoding: {
+					x: { field: 'distance', bin: true },
+					y: { aggregate: 'count' },
+					color: { value: 'goldenrod' }
+				}
+			}
+		]
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
