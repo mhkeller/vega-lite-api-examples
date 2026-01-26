c15910a70da77229f6cfb34e184a6f3f4bd4cf70
mhkeller <code@mhkeller.com>
2026-01-25 21:46:45 -0500

    add line_max_year

--- src/line_max_year.vl.js
+++ src/line_max_year.vl.js
@@ -5,6 +5,17 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.markLine()
+		.data('data/seattle-weather.csv')
+		.encode(
+			vl.x().timeUnit('yearmonth').field('date'),
+			vl.y().aggregate('max').field('temp_max')
+		)
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
