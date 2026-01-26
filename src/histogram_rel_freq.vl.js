993626755d4ed47e535875fd891259cb0cbaf125
mhkeller <code@mhkeller.com>
2026-01-25 21:35:02 -0500

    add histogram_rel_freq

--- src/histogram_rel_freq.vl.js
+++ src/histogram_rel_freq.vl.js
@@ -5,6 +5,45 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.markBar({ tooltip: true })
+		.description(
+			'Relative frequency histogram. The data is binned with first transform. The number of values per bin and the total number are calculated in the second and third transform to calculate the relative frequency in the last transformation step.'
+		)
+		.data('data/cars.json')
+		.transform(
+			{ bin: true, field: 'Horsepower', as: 'bin_Horsepwoer' },
+			{
+				aggregate: [{ op: 'count', as: 'Count' }],
+				groupby: ['bin_Horsepwoer', 'bin_Horsepwoer_end']
+			},
+			{
+				joinaggregate: [
+					{ op: 'sum', field: 'Count', as: 'TotalCount' }
+				]
+			},
+			{
+				calculate: 'datum.Count/datum.TotalCount',
+				as: 'PercentOfTotal'
+			}
+		)
+		.encode(
+			vl
+				.x()
+				.field('bin_Horsepwoer')
+				.title('Horsepower')
+				.bin({ binned: true }),
+			vl.x2().field('bin_Horsepwoer_end'),
+			vl
+				.y()
+				.fieldQ('PercentOfTotal')
+				.title('Relative Frequency')
+				.axis({ format: '.1~%' })
+		)
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
