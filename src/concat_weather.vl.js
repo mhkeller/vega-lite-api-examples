a032c0c66cc09b2f56fb8fdd1a87547079d60ad3
mhkeller <code@mhkeller.com>
2026-01-26 08:48:27 -0500

    add concat_weather

--- src/concat_weather.vl.js
+++ src/concat_weather.vl.js
@@ -5,6 +5,32 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.concat(
+			vl.markBar().encode(
+				vl.x().timeUnit('month').fieldO('date'),
+				vl.y().aggregate('mean').field('precipitation')
+			),
+			vl.markBar().encode(
+				vl.x().timeUnit('month').fieldO('date'),
+				vl.y().aggregate('median').field('precipitation')
+			),
+			vl.markPoint().encode(
+				vl.x().field('temp_min').bin(true),
+				vl.y().field('temp_max').bin(true),
+				vl.size().aggregate('count')
+			)
+		)
+		.description(
+			'Two horizonally concatenated charts that show a histogram of precipitation in Seattle and the relationship between min and max temperature.'
+		)
+		.data('data/weather.csv')
+		.transform({ filter: "datum.location === 'Seattle'" })
+		.columns(2)
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
