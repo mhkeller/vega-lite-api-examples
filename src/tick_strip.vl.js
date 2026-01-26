259e45f8b6c2a5e4ab7e8cb2e4ebc005b856de25
mhkeller <code@mhkeller.com>
2026-01-25 21:37:54 -0500

    add tick_strip

--- src/tick_strip.vl.js
+++ src/tick_strip.vl.js
@@ -5,6 +5,20 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.markTick()
+		.description(
+			'Shows the relationship between horsepower and the number of cylinders using tick marks.'
+		)
+		.data('data/cars.json')
+		.encode(
+			vl.x().fieldQ('Horsepower'),
+			vl.y().fieldO('Cylinders')
+		)
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
