ce5ccf1549d1f0f21affd636dc32d5e318cb5e04
mhkeller <code@mhkeller.com>
2026-01-26 08:58:43 -0500

    add interactive_brush

--- src/interactive_brush.vl.js
+++ src/interactive_brush.vl.js
@@ -5,6 +5,34 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		description: 'Drag out a rectangular brush to highlight points.',
+		data: { url: 'data/cars.json' },
+		params: [
+			{
+				name: 'brush',
+				select: 'interval',
+				value: { x: [55, 160], y: [13, 37] }
+			}
+		],
+		mark: { type: 'point' },
+		encoding: {
+			x: { field: 'Horsepower', type: 'quantitative' },
+			y: { field: 'Miles_per_Gallon', type: 'quantitative' },
+			color: {
+				condition: {
+					param: 'brush',
+					field: 'Cylinders',
+					type: 'ordinal'
+				},
+				value: 'grey'
+			}
+		}
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
