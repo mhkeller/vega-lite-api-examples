e29daacd22b96160b3f220c4ecb88dbc54bc41ca
mhkeller <code@mhkeller.com>
2026-01-26 15:51:22 -0500

    add layer_rect_extent

--- src/layer_rect_extent.vl.js
+++ src/layer_rect_extent.vl.js
@@ -5,6 +5,40 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		description:
+			'A scatterplot showing horsepower and miles per gallons for various cars.',
+		data: { url: 'data/cars.json' },
+		layer: [
+			{
+				mark: { type: 'point' },
+				encoding: {
+					x: { field: 'Horsepower', type: 'quantitative' },
+					y: { field: 'Miles_per_Gallon', type: 'quantitative', title: 'Miles per Gallon' }
+				}
+			},
+			{
+				mark: { type: 'rule' },
+				encoding: { y: { aggregate: 'min', field: 'Miles_per_Gallon' } }
+			},
+			{
+				mark: { type: 'rule' },
+				encoding: { y: { aggregate: 'max', field: 'Miles_per_Gallon' } }
+			},
+			{
+				mark: { type: 'rect' },
+				encoding: {
+					y: { aggregate: 'max', field: 'Miles_per_Gallon' },
+					y2: { aggregate: 'min', field: 'Miles_per_Gallon' },
+					opacity: { value: 0.2 }
+				}
+			}
+		]
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
