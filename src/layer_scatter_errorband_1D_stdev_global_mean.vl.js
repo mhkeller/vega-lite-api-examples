96000d1e10d2f5d85ae58c90f7c0a3e776e2b076
mhkeller <code@mhkeller.com>
2026-01-26 15:54:45 -0500

    add layer_scatter_errorband_1D_stdev_global_mean

--- src/layer_scatter_errorband_1D_stdev_global_mean.vl.js
+++ src/layer_scatter_errorband_1D_stdev_global_mean.vl.js
@@ -5,6 +5,36 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		description:
+			'A scatterplot showing horsepower and miles per gallons for various cars.',
+		data: { url: 'data/cars.json' },
+		layer: [
+			{
+				mark: { type: 'point' },
+				encoding: {
+					x: { field: 'Horsepower', type: 'quantitative' },
+					y: { field: 'Miles_per_Gallon', type: 'quantitative' }
+				}
+			},
+			{
+				mark: { type: 'errorband', extent: 'stdev', opacity: 0.2 },
+				encoding: {
+					y: { field: 'Miles_per_Gallon', type: 'quantitative', title: 'Miles per Gallon' }
+				}
+			},
+			{
+				mark: { type: 'rule' },
+				encoding: {
+					y: { field: 'Miles_per_Gallon', type: 'quantitative', aggregate: 'mean' }
+				}
+			}
+		]
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
