fb939373b8d5fa7ee4de38bae454954edd6b1c82
mhkeller <code@mhkeller.com>
2026-01-25 22:15:53 -0500

    add window_top_k

--- src/window_top_k.vl.js
+++ src/window_top_k.vl.js
@@ -5,6 +5,55 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.markBar()
+		.description(
+			'A bar graph showing the scores of the top 5 students. This shows an example of the window transform, for how the top K (5) can be filtered, and also how a rank can be computed for each student.'
+		)
+		.data({
+			values: [
+				{ student: 'A', score: 100 },
+				{ student: 'B', score: 56 },
+				{ student: 'C', score: 88 },
+				{ student: 'D', score: 65 },
+				{ student: 'E', score: 45 },
+				{ student: 'F', score: 23 },
+				{ student: 'G', score: 66 },
+				{ student: 'H', score: 67 },
+				{ student: 'I', score: 13 },
+				{ student: 'J', score: 12 },
+				{ student: 'K', score: 50 },
+				{ student: 'L', score: 78 },
+				{ student: 'M', score: 66 },
+				{ student: 'N', score: 30 },
+				{ student: 'O', score: 97 },
+				{ student: 'P', score: 75 },
+				{ student: 'Q', score: 24 },
+				{ student: 'R', score: 42 },
+				{ student: 'S', score: 76 },
+				{ student: 'T', score: 78 },
+				{ student: 'U', score: 21 },
+				{ student: 'V', score: 46 }
+			]
+		})
+		.transform(
+			{
+				window: [{ op: 'rank', as: 'rank' }],
+				sort: [{ field: 'score', order: 'descending' }]
+			},
+			{ filter: 'datum.rank <= 5' }
+		)
+		.encode(
+			vl.x().fieldQ('score'),
+			vl
+				.y()
+				.fieldN('student')
+				.sort({ field: 'score', op: 'average', order: 'descending' })
+		)
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
