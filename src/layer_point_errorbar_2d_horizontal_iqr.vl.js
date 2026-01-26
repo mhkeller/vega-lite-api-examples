b8f62cda46f3cde9491e7fc6b330b08c086b5aed
mhkeller <code@mhkeller.com>
2026-01-26 15:50:18 -0500

    add layer_point_errorbar_2d_horizontal_iqr

--- src/layer_point_errorbar_2d_horizontal_iqr.vl.js
+++ src/layer_point_errorbar_2d_horizontal_iqr.vl.js
@@ -5,6 +5,29 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: { url: 'data/barley.json' },
+		layer: [
+			{
+				mark: { type: 'errorbar', extent: 'iqr' },
+				encoding: {
+					x: { field: 'yield', type: 'quantitative', scale: { zero: false } },
+					y: { field: 'variety', type: 'ordinal' }
+				}
+			},
+			{
+				mark: { type: 'point', filled: true, color: 'black' },
+				encoding: {
+					x: { field: 'yield', type: 'quantitative', aggregate: 'median' },
+					y: { field: 'variety', type: 'ordinal' }
+				}
+			}
+		]
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
