2ce43e55b0a072092ec4ee8d2a3a47884dfc0a16
mhkeller <code@mhkeller.com>
2026-01-26 17:49:21 -0500

    add repeat_splom

--- src/repeat_splom.vl.js
+++ src/repeat_splom.vl.js
@@ -5,6 +5,45 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: { url: 'data/penguins.json' },
+		repeat: {
+			row: [
+				'Beak Length (mm)',
+				'Beak Depth (mm)',
+				'Flipper Length (mm)',
+				'Body Mass (g)'
+			],
+			column: [
+				'Body Mass (g)',
+				'Flipper Length (mm)',
+				'Beak Depth (mm)',
+				'Beak Length (mm)'
+			]
+		},
+		spec: {
+			width: 150,
+			height: 150,
+			mark: { type: 'point' },
+			encoding: {
+				x: {
+					field: { repeat: 'column' },
+					type: 'quantitative',
+					scale: { zero: false }
+				},
+				y: {
+					field: { repeat: 'row' },
+					type: 'quantitative',
+					scale: { zero: false }
+				},
+				color: { field: 'Species', type: 'nominal' }
+			}
+		}
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
