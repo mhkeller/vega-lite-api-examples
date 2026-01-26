0d3aabf40a9865130fa83fd5a5c5b98225872a5b
mhkeller <code@mhkeller.com>
2026-01-26 15:51:22 -0500

    add layer_point_errorbar_pre_aggregated_asymmetric_error

--- src/layer_point_errorbar_pre_aggregated_asymmetric_error.vl.js
+++ src/layer_point_errorbar_pre_aggregated_asymmetric_error.vl.js
@@ -5,6 +5,43 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: {
+			values: [
+				{ yield_error: 7.5522, yield_error2: -10.5522, yield_center: 32.4, variety: 'Glabron' },
+				{ yield_error: 6.9775, yield_error2: -3.9775, yield_center: 30.96667, variety: 'Manchuria' },
+				{ yield_error: 3.9167, yield_error2: -0.9167, yield_center: 33.966665, variety: 'No. 457' },
+				{ yield_error: 11.9732, yield_error2: -15.9732, yield_center: 30.45, variety: 'No. 462' }
+			]
+		},
+		layer: [
+			{
+				mark: { type: 'errorbar' },
+				encoding: {
+					x: {
+						field: 'yield_center',
+						type: 'quantitative',
+						scale: { zero: false },
+						title: 'yield'
+					},
+					xError: { field: 'yield_error' },
+					xError2: { field: 'yield_error2' },
+					y: { field: 'variety', type: 'ordinal' }
+				}
+			},
+			{
+				mark: { type: 'point', filled: true, color: 'black' },
+				encoding: {
+					x: { field: 'yield_center', type: 'quantitative' },
+					y: { field: 'variety', type: 'ordinal' }
+				}
+			}
+		]
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
