c728a008bdafed4466f02fa65b7f8471c59054dc
mhkeller <code@mhkeller.com>
2026-01-26 09:04:23 -0500

    add interactive_paintbrush_color

--- src/interactive_paintbrush_color.vl.js
+++ src/interactive_paintbrush_color.vl.js
@@ -5,6 +5,31 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		description: 'Select multiple points with the shift key.',
+		data: { url: 'data/cars.json' },
+		params: [
+			{ name: 'paintbrush', select: { type: 'point', on: 'mouseover' } }
+		],
+		mark: { type: 'circle' },
+		encoding: {
+			x: { field: 'Horsepower', type: 'quantitative' },
+			y: { field: 'Miles_per_Gallon', type: 'quantitative' },
+			color: {
+				condition: {
+					param: 'paintbrush',
+					field: 'Cylinders',
+					type: 'ordinal'
+				},
+				value: 'grey'
+			},
+			size: { value: 75 }
+		}
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
