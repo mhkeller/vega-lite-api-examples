16eeda191089fe10e63c4f149710062ed2ac7874
mhkeller <code@mhkeller.com>
2026-01-26 09:07:56 -0500

    add interactive_splom

--- src/interactive_splom.vl.js
+++ src/interactive_splom.vl.js
@@ -5,6 +5,60 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		repeat: {
+			row: ['Horsepower', 'Acceleration', 'Miles_per_Gallon'],
+			column: ['Miles_per_Gallon', 'Acceleration', 'Horsepower']
+		},
+		spec: {
+			data: { url: 'data/cars.json' },
+			mark: { type: 'point' },
+			params: [
+				{
+					name: 'brush',
+					select: {
+						type: 'interval',
+						resolve: 'union',
+						on: '[mousedown[event.shiftKey], window:mouseup] > window:mousemove!',
+						translate:
+							'[mousedown[event.shiftKey], window:mouseup] > window:mousemove!',
+						zoom: 'wheel![event.shiftKey]'
+					}
+				},
+				{
+					name: 'grid',
+					select: {
+						type: 'interval',
+						resolve: 'global',
+						translate:
+							'[mousedown[!event.shiftKey], window:mouseup] > window:mousemove!',
+						zoom: 'wheel![!event.shiftKey]'
+					},
+					bind: 'scales'
+				}
+			],
+			encoding: {
+				x: { field: { repeat: 'column' }, type: 'quantitative' },
+				y: {
+					field: { repeat: 'row' },
+					type: 'quantitative',
+					axis: { minExtent: 30 }
+				},
+				color: {
+					condition: {
+						param: 'brush',
+						field: 'Origin',
+						type: 'nominal'
+					},
+					value: 'grey'
+				}
+			}
+		}
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
