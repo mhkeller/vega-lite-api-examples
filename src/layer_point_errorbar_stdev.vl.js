57514803e70130612dda81760b511bddaf97f971
mhkeller <code@mhkeller.com>
2026-01-26 15:50:48 -0500

    add layer_point_errorbar_stdev

--- src/layer_point_errorbar_stdev.vl.js
+++ src/layer_point_errorbar_stdev.vl.js
@@ -5,6 +5,35 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: { url: 'data/barley.json' },
+		encoding: { y: { field: 'variety', type: 'ordinal' } },
+		layer: [
+			{
+				mark: { type: 'point', filled: true },
+				encoding: {
+					x: {
+						aggregate: 'mean',
+						field: 'yield',
+						type: 'quantitative',
+						scale: { zero: false },
+						title: 'Barley Yield'
+					},
+					color: { value: 'black' }
+				}
+			},
+			{
+				mark: { type: 'errorbar', extent: 'stdev' },
+				encoding: {
+					x: { field: 'yield', type: 'quantitative', title: 'Barley Yield' }
+				}
+			}
+		]
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
