d8d6177582248f7f4b1e4feb0d1175dde591dd99
mhkeller <code@mhkeller.com>
2026-01-26 09:05:26 -0500

    add interactive_line_point_hover

--- src/interactive_line_point_hover.vl.js
+++ src/interactive_line_point_hover.vl.js
@@ -5,6 +5,52 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		description:
+			'Stock prices of 5 Tech Companies over Time, with a point marker on hover. Note that the hidden markers are intentionally bigger to make it easier for readers to hover.',
+		data: { url: 'data/stocks.csv' },
+		encoding: {
+			x: { field: 'date', type: 'temporal' },
+			y: { field: 'price', type: 'quantitative' },
+			color: { field: 'symbol', type: 'nominal' }
+		},
+		layer: [
+			{ mark: { type: 'line' } },
+			{
+				params: [
+					{
+						name: 'hover',
+						select: {
+							type: 'point',
+							on: 'mouseover',
+							clear: 'mouseout'
+						}
+					}
+				],
+				mark: { type: 'circle', tooltip: true },
+				encoding: {
+					opacity: {
+						condition: {
+							test: { param: 'hover', empty: false },
+							value: 1
+						},
+						value: 0
+					},
+					size: {
+						condition: {
+							test: { param: 'hover', empty: false },
+							value: 48
+						},
+						value: 100
+					}
+				}
+			}
+		]
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
