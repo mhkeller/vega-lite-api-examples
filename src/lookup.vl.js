bef03fea0ab7ae8d907857f2d050fe64e8728142
mhkeller <code@mhkeller.com>
2026-01-25 22:15:29 -0500

    add lookup

--- src/lookup.vl.js
+++ src/lookup.vl.js
@@ -5,6 +5,27 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.markBar()
+		.data('data/lookup_groups.csv')
+		.transform(
+			vl
+				.lookup('person')
+				.from(
+					vl
+						.data('data/lookup_people.csv')
+						.key('name')
+						.fields(['age', 'height'])
+				)
+		)
+		.encode(
+			vl.x().field('group'),
+			vl.y().field('age').aggregate('mean')
+		)
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
