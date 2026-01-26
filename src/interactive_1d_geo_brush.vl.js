636b16484d1c09c7a7e855dd6235a74f3b0547dc
mhkeller <code@mhkeller.com>
2026-01-26 09:03:20 -0500

    add interactive_1d_geo_brush

--- src/interactive_1d_geo_brush.vl.js
+++ src/interactive_1d_geo_brush.vl.js
@@ -5,6 +5,51 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		width: 500,
+		height: 300,
+		projection: { type: 'albersUsa' },
+		layer: [
+			{
+				data: {
+					url: 'data/us-10m.json',
+					format: { type: 'topojson', feature: 'states' }
+				},
+				mark: { type: 'geoshape', fill: 'lightgray', stroke: 'white' }
+			},
+			{
+				data: { url: 'data/airports.csv' },
+				transform: [
+					{ filter: "datum.state !== 'PR' && datum.state !== 'VI'" }
+				],
+				params: [
+					{
+						name: 'brush',
+						select: { type: 'interval', encodings: ['latitude'] },
+						value: { latitude: [45, 51.5] }
+					}
+				],
+				mark: { type: 'circle' },
+				encoding: {
+					longitude: { field: 'longitude', type: 'quantitative' },
+					latitude: { field: 'latitude', type: 'quantitative' },
+					color: {
+						condition: {
+							param: 'brush',
+							empty: false,
+							value: 'goldenrod'
+						},
+						value: 'steelblue'
+					},
+					size: { value: 10 }
+				}
+			}
+		]
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
