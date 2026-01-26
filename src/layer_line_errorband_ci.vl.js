c68c47dff9d0d90a956ef46c5a04d52f6d36ec0c
mhkeller <code@mhkeller.com>
2026-01-26 09:44:09 -0500

    add layer_line_errorband_ci

--- src/layer_line_errorband_ci.vl.js
+++ src/layer_line_errorband_ci.vl.js
@@ -5,6 +5,30 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: { url: 'data/cars.json' },
+		encoding: { x: { field: 'Year', timeUnit: 'year' } },
+		layer: [
+			{
+				mark: { type: 'errorband', extent: 'ci' },
+				encoding: {
+					y: {
+						field: 'Miles_per_Gallon',
+						type: 'quantitative',
+						title: 'Mean of Miles per Gallon (95% CIs)'
+					}
+				}
+			},
+			{
+				mark: { type: 'line' },
+				encoding: { y: { aggregate: 'mean', field: 'Miles_per_Gallon' } }
+			}
+		]
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
