31b8f1ee59c9d077f2e4777d191a69dee986062c
mhkeller <code@mhkeller.com>
2026-01-26 08:57:38 -0500

    add geo_sphere

--- src/geo_sphere.vl.js
+++ src/geo_sphere.vl.js
@@ -5,6 +5,25 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		width: 200,
+		height: 200,
+		projection: { type: 'orthographic', scale: 100, translate: [100, 100] },
+		layer: [
+			{
+				data: { sphere: true },
+				mark: { type: 'geoshape', fill: 'aliceblue' }
+			},
+			{
+				data: { graticule: true },
+				mark: { type: 'geoshape', stroke: 'black', strokeWidth: 0.5 }
+			}
+		]
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
