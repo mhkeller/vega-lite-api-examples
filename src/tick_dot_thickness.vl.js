358cb925658d5c1edbc014356f52e832a3a41627
mhkeller <code@mhkeller.com>
2026-01-25 21:37:37 -0500

    add tick_dot_thickness

--- src/tick_dot_thickness.vl.js
+++ src/tick_dot_thickness.vl.js
@@ -5,6 +5,15 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.markTick()
+		.data('data/cars.json')
+		.encode(vl.x().fieldQ('Horsepower'))
+		.config({ tick: { thickness: 2, bandSize: 10 } })
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
