a8484da4bcb623c0bf9746ec8c662ac49c2cf53a
mhkeller <code@mhkeller.com>
2026-01-26 09:43:36 -0500

    add layer_line_datum_rule

--- src/layer_line_datum_rule.vl.js
+++ src/layer_line_datum_rule.vl.js
@@ -5,6 +5,28 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		layer: [
+			{
+				data: { url: 'data/stocks.csv' },
+				mark: { type: 'line' },
+				encoding: {
+					x: { field: 'date', type: 'temporal' },
+					y: { field: 'price', type: 'quantitative' },
+					color: { field: 'symbol', type: 'nominal' }
+				}
+			},
+			{
+				data: { values: [{}] },
+				mark: { type: 'rule', strokeDash: [2, 2], size: 2 },
+				encoding: { y: { datum: 300 } }
+			}
+		]
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
