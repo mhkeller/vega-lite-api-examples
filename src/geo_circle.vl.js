898d0bc9e675c14be5169a1cda74c36c9f6dd360
mhkeller <code@mhkeller.com>
2026-01-26 08:57:07 -0500

    add geo_circle

--- src/geo_circle.vl.js
+++ src/geo_circle.vl.js
@@ -5,6 +5,24 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	const spec = vl
+		.markCircle()
+		.width(500)
+		.height(300)
+		.data('data/zipcodes.csv')
+		.transform({ calculate: "substring(datum.zip_code, 0, 1)", as: 'digit' })
+		.encode(
+			vl.longitude().fieldQ('longitude'),
+			vl.latitude().fieldQ('latitude'),
+			vl.size().value(1),
+			vl.color().fieldN('digit')
+		)
+		.toSpec();
+	spec.projection = { type: 'albersUsa' };
+	return spec;
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
