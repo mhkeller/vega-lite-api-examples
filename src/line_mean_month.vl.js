c30bfb8a07dcf13f313dd134368d670567c06301
mhkeller <code@mhkeller.com>
2026-01-25 21:46:11 -0500

    add line_mean_month

--- src/line_mean_month.vl.js
+++ src/line_mean_month.vl.js
@@ -5,6 +5,17 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.markLine()
+		.data('data/seattle-weather.csv')
+		.encode(
+			vl.x().timeUnit('month').field('date'),
+			vl.y().aggregate('mean').field('precipitation')
+		)
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
