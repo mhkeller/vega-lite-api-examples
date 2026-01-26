f3ee98b496dc5b96359c81ee52bd17c669338917
mhkeller <code@mhkeller.com>
2026-01-26 17:45:26 -0500

    add rect_heatmap_weather

--- src/rect_heatmap_weather.vl.js
+++ src/rect_heatmap_weather.vl.js
@@ -5,6 +5,35 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: { url: 'data/seattle-weather.csv' },
+		title: 'Daily Max Temperatures (C) in Seattle, WA',
+		config: {
+			view: { strokeWidth: 0, step: 13 },
+			axis: { domain: false }
+		},
+		mark: { type: 'rect' },
+		encoding: {
+			x: {
+				field: 'date',
+				timeUnit: 'date',
+				type: 'ordinal',
+				title: 'Day',
+				axis: { labelAngle: 0, format: '%e' }
+			},
+			y: { field: 'date', timeUnit: 'month', type: 'ordinal', title: 'Month' },
+			color: {
+				field: 'temp_max',
+				aggregate: 'max',
+				type: 'quantitative',
+				legend: { title: null }
+			}
+		}
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
