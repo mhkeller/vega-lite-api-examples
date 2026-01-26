f2f1d65f1817b18252e783b3aeb0d0740f1b6b5e
mhkeller <code@mhkeller.com>
2026-01-26 17:44:47 -0500

    add line_shape_overlay

--- src/line_shape_overlay.vl.js
+++ src/line_shape_overlay.vl.js
@@ -5,6 +5,21 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		description: "Google's stock price over time.",
+		data: { url: 'data/stocks.csv' },
+		transform: [{ filter: "datum.symbol==='GOOG'" }],
+		mark: { type: 'line' },
+		encoding: {
+			x: { field: 'date', type: 'temporal' },
+			y: { field: 'price', type: 'quantitative' },
+			shape: { value: 'square' }
+		}
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
