f80b635fcfc926281dd0d89d7965e8be1a1e3ea0
mhkeller <code@mhkeller.com>
2026-01-26 09:03:57 -0500

    add interactive_point_init

--- src/interactive_point_init.vl.js
+++ src/interactive_point_init.vl.js
@@ -5,6 +5,37 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: { url: 'data/cars.json' },
+		transform: [{ calculate: 'year(datum.Year)', as: 'Year' }],
+		params: [
+			{
+				name: 'CylYr',
+				select: { type: 'point', fields: ['Cylinders', 'Year'] },
+				value: [
+					{ Cylinders: 4, Year: 1981 },
+					{ Cylinders: 8, Year: 1972 }
+				]
+			}
+		],
+		mark: { type: 'circle' },
+		encoding: {
+			x: { field: 'Horsepower', type: 'quantitative' },
+			y: { field: 'Miles_per_Gallon', type: 'quantitative' },
+			color: {
+				condition: {
+					param: 'CylYr',
+					field: 'Origin',
+					type: 'nominal'
+				},
+				value: 'grey'
+			}
+		}
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
---
+++ test/interactive_point_init.test.js
@@ -0,0 +1,32 @@
+/* globals describe it */
+import * as assert from 'assert';
+import { readFileSync } from 'fs';
+
+import fn from '../src/interactive_point_init.vl.js';
+
+function convertMarkValues (obj) {
+	for (const key in obj) {
+		if (typeof obj[key] === 'object') {
+			convertMarkValues(obj[key]);
+		}
+		if (key === 'mark' && typeof obj[key] === 'string') {
+			obj.mark = { type: obj[key] };
+		}
+	}
+
+	return obj;
+}
+
+const name = 'interactive_point_init';
+
+let spec = JSON.parse(readFileSync(`./specs/${name}.vl.json`, 'utf-8'));
+
+// Changes mark fields that are nested in any objects from a string to an object, which is what vega-lite-api creates
+spec = convertMarkValues(spec);
+
+describe(`${name}.vg.js`, () => {
+	it(`should match spec`, () => {
+		const actual = fn();
+		assert.deepStrictEqual(actual, spec);
+	});
+});
