609cf821866a1a74a37c3c3c6e106cd174835561
mhkeller <code@mhkeller.com>
2026-01-26 17:46:39 -0500

    add interactive_multi_line_pivot_tooltip

--- src/interactive_multi_line_pivot_tooltip.vl.js
+++ src/interactive_multi_line_pivot_tooltip.vl.js
@@ -5,6 +5,60 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: { url: 'data/stocks.csv' },
+		width: 400,
+		height: 300,
+		encoding: { x: { field: 'date', type: 'temporal' } },
+		layer: [
+			{
+				encoding: {
+					color: { field: 'symbol', type: 'nominal' },
+					y: { field: 'price', type: 'quantitative' }
+				},
+				layer: [
+					{ mark: { type: 'line' } },
+					{
+						transform: [{ filter: { param: 'hover', empty: false } }],
+						mark: { type: 'point' }
+					}
+				]
+			},
+			{
+				transform: [{ pivot: 'symbol', value: 'price', groupby: ['date'] }],
+				mark: { type: 'rule' },
+				encoding: {
+					opacity: {
+						condition: { value: 0.3, param: 'hover', empty: false },
+						value: 0
+					},
+					tooltip: [
+						{ field: 'AAPL', type: 'quantitative' },
+						{ field: 'AMZN', type: 'quantitative' },
+						{ field: 'GOOG', type: 'quantitative' },
+						{ field: 'IBM', type: 'quantitative' },
+						{ field: 'MSFT', type: 'quantitative' }
+					]
+				},
+				params: [
+					{
+						name: 'hover',
+						select: {
+							type: 'point',
+							fields: ['date'],
+							nearest: true,
+							on: 'mouseover',
+							clear: 'mouseout'
+						}
+					}
+				]
+			}
+		]
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
