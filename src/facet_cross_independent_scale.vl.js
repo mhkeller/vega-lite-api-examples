9e67f1e52b350987074d8303f59e8d5b9e69b529
mhkeller <code@mhkeller.com>
2026-01-26 08:53:22 -0500

    add facet_cross_independent_scale

--- src/facet_cross_independent_scale.vl.js
+++ src/facet_cross_independent_scale.vl.js
@@ -5,6 +5,32 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: {
+			values: [
+				{ r: 'r1', c: 'c1', a: 'a1', b: 'b1' },
+				{ r: 'r1', c: 'c1', a: 'a2', b: 'b2' },
+				{ r: 'r2', c: 'c2', a: 'a1', b: 'b1' },
+				{ r: 'r3', c: 'c2', a: 'a3', b: 'b2' }
+			]
+		},
+		facet: {
+			row: { field: 'r' },
+			column: { field: 'c' }
+		},
+		spec: {
+			mark: { type: 'rect' },
+			encoding: {
+				y: { field: 'b', type: 'nominal' },
+				x: { field: 'a', type: 'nominal' }
+			}
+		},
+		resolve: { scale: { x: 'independent', y: 'independent' } }
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
