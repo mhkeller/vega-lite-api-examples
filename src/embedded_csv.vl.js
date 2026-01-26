1f135da3c4718c50c8aec2d7691cd14413955936
mhkeller <code@mhkeller.com>
2026-01-25 21:52:17 -0500

    add embedded_csv

--- src/embedded_csv.vl.js
+++ src/embedded_csv.vl.js
@@ -5,6 +5,17 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.markPoint()
+		.data({
+			values: 'a\n1\n2\n3\n4',
+			format: { type: 'csv' }
+		})
+		.encode(vl.y().fieldQ('a'))
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
