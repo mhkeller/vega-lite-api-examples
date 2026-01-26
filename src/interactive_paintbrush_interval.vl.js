1dc6843d4f7559c377d1d06d808e48d942d45236
mhkeller <code@mhkeller.com>
2026-01-26 09:04:23 -0500

    add interactive_paintbrush_interval

--- src/interactive_paintbrush_interval.vl.js
+++ src/interactive_paintbrush_interval.vl.js
@@ -5,6 +5,24 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		description: 'Select multiple points by dragging an interval.',
+		data: { url: 'data/cars.json' },
+		params: [{ name: 'paintbrush', select: 'interval' }],
+		mark: { type: 'point' },
+		encoding: {
+			x: { field: 'Horsepower', type: 'quantitative' },
+			y: { field: 'Miles_per_Gallon', type: 'quantitative' },
+			size: {
+				condition: { param: 'paintbrush', value: 300 },
+				value: 50
+			}
+		}
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
