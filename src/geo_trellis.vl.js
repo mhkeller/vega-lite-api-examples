7bcfce2c995dab00a3d9d2e73ff80d996cfd1b6e
mhkeller <code@mhkeller.com>
2026-01-25 22:33:21 -0500

    add geo_trellis

--- src/geo_trellis.vl.js
+++ src/geo_trellis.vl.js
@@ -5,6 +5,32 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	const spec = vl
+		.markGeoshape()
+		.width(500)
+		.height(300)
+		.data('data/income.json')
+		.transform(
+			vl.lookup('id').from(
+				vl
+					.data({
+						url: 'data/us-10m.json',
+						format: { type: 'topojson', feature: 'states' }
+					})
+					.key('id')
+			).as('geo')
+		)
+		.encode(
+			vl.shape().field('geo').type('geojson'),
+			vl.color().fieldQ('pct'),
+			vl.row().field('group')
+		)
+		.toSpec();
+	spec.projection = { type: 'albersUsa' };
+	return spec;
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
