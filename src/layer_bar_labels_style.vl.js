7fa98fbd5b1bbe85c6940f0ee6e818ccd5b9a3a4
mhkeller <code@mhkeller.com>
2026-01-26 09:40:13 -0500

    add layer_bar_labels_style

--- src/layer_bar_labels_style.vl.js
+++ src/layer_bar_labels_style.vl.js
@@ -5,6 +5,39 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: {
+			values: [
+				{ a: 'A', b: 28 },
+				{ a: 'B', b: 55 },
+				{ a: 'C', b: 43 }
+			]
+		},
+		layer: [
+			{
+				mark: { type: 'bar' },
+				encoding: {
+					y: { field: 'a', type: 'nominal' },
+					x: { field: 'b', type: 'quantitative' }
+				}
+			},
+			{
+				mark: { type: 'text', style: 'label' },
+				encoding: {
+					y: { field: 'a', type: 'nominal' },
+					x: { field: 'b', type: 'quantitative' },
+					text: { field: 'b', type: 'quantitative' }
+				}
+			}
+		],
+		config: {
+			style: { label: { align: 'left', baseline: 'middle', dx: 3 } }
+		}
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
