7c634261a4b05423b2be2888cca2079a7af1268e
mhkeller <code@mhkeller.com>
2026-01-26 09:04:51 -0500

    add interactive_paintbrush_simple_false

--- src/interactive_paintbrush_simple_false.vl.js
+++ src/interactive_paintbrush_simple_false.vl.js
@@ -5,6 +5,37 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: { url: 'data/cars.json' },
+		params: [
+			{ name: 'toggleOrigin', bind: { input: 'checkbox' } },
+			{
+				name: 'paintbrush',
+				select: { type: 'point', on: 'mouseover' }
+			}
+		],
+		mark: { type: 'point' },
+		encoding: {
+			x: { field: 'Horsepower', type: 'quantitative' },
+			y: { field: 'Miles_per_Gallon', type: 'quantitative' },
+			color: {
+				condition: {
+					param: 'toggleOrigin',
+					field: 'Origin',
+					type: 'nominal'
+				},
+				value: 'steelblue'
+			},
+			size: {
+				condition: { param: 'paintbrush', empty: false, value: 300 },
+				value: 50
+			}
+		}
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
