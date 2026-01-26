ae3d8c74f1d6fabfaec1b73bbd50aa6097c3befd
mhkeller <code@mhkeller.com>
2026-01-26 09:43:02 -0500

    add layer_line_color_rule

--- src/layer_line_color_rule.vl.js
+++ src/layer_line_color_rule.vl.js
@@ -5,6 +5,32 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		description: 'Stock prices of 5 Tech Companies over Time with Averages.',
+		data: { url: 'data/stocks.csv' },
+		layer: [
+			{
+				mark: { type: 'line' },
+				encoding: {
+					x: { field: 'date', type: 'temporal' },
+					y: { field: 'price', type: 'quantitative' },
+					color: { field: 'symbol', type: 'nominal' }
+				}
+			},
+			{
+				mark: { type: 'rule' },
+				encoding: {
+					y: { field: 'price', aggregate: 'mean' },
+					size: { value: 2 },
+					color: { field: 'symbol' }
+				}
+			}
+		]
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
