e8b1bdb8d784a3c46ac7cb36cd8a130d564a5446
mhkeller <code@mhkeller.com>
2026-01-26 09:41:35 -0500

    add layer_circle_independent_color

--- src/layer_circle_independent_color.vl.js
+++ src/layer_circle_independent_color.vl.js
@@ -5,6 +5,44 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		description:
+			'Cars from Japan compared to all cars. Demonstrates independent legends in layers.',
+		data: { url: 'data/cars.json' },
+		layer: [
+			{
+				mark: { type: 'circle' },
+				encoding: {
+					x: { bin: true, field: 'Acceleration', type: 'quantitative' },
+					y: { bin: true, field: 'Horsepower', type: 'quantitative' },
+					size: {
+						aggregate: 'count',
+						type: 'quantitative',
+						title: 'All Cars Count'
+					}
+				}
+			},
+			{
+				transform: [{ filter: "datum.Origin === 'Japan'" }],
+				mark: { type: 'circle' },
+				encoding: {
+					x: { bin: true, field: 'Acceleration', type: 'quantitative' },
+					y: { bin: true, field: 'Horsepower', type: 'quantitative' },
+					size: {
+						aggregate: 'count',
+						type: 'quantitative',
+						title: 'Cars from Japan Count'
+					},
+					color: { value: 'firebrick' }
+				}
+			}
+		],
+		resolve: { legend: { size: 'independent' } }
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
