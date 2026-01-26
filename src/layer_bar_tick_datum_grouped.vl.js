d3fbeb74d885dd113a5d67ae17a2b16e896fbe60
mhkeller <code@mhkeller.com>
2026-01-26 09:40:50 -0500

    add layer_bar_tick_datum_grouped

--- src/layer_bar_tick_datum_grouped.vl.js
+++ src/layer_bar_tick_datum_grouped.vl.js
@@ -5,6 +5,36 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		description:
+			'A vertical box plot showing median, min, and max body mass of penguins.',
+		data: { url: 'data/cars.json' },
+		encoding: {
+			x: { field: 'Cylinders', type: 'nominal' },
+			xOffset: { field: 'Origin', type: 'nominal' }
+		},
+		layer: [
+			{
+				mark: { type: 'bar' },
+				encoding: {
+					y: {
+						aggregate: 'mean',
+						field: 'Acceleration',
+						type: 'quantitative'
+					},
+					color: { field: 'Origin', type: 'nominal' }
+				}
+			},
+			{
+				mark: { type: 'tick', color: 'black' },
+				encoding: { y: { datum: 10 } }
+			}
+		]
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
