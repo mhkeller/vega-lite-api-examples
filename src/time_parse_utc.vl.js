90c4d4dc41d03a73cc4094653f080ca960100b83
mhkeller <code@mhkeller.com>
2026-01-25 22:18:07 -0500

    add time_parse_utc

--- src/time_parse_utc.vl.js
+++ src/time_parse_utc.vl.js
@@ -5,6 +5,17 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.markPoint()
+		.description('Parsing a date in utc and outputting utc.')
+		.data({
+			values: [{ date: '2011-10-10' }, { date: '2011-10-12' }]
+		})
+		.encode(vl.y().timeUnit('utchours').fieldO('date').title('time'))
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
