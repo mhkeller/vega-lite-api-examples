b141b7ca0fad88468e2e57e42e4feeb9c75b1c4f
mhkeller <code@mhkeller.com>
2026-01-26 09:10:49 -0500

    add layer_bar_labels_grey

--- src/layer_bar_labels_grey.vl.js
+++ src/layer_bar_labels_grey.vl.js
@@ -5,6 +5,36 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		width: 200,
+		height: { step: 16 },
+		data: { url: 'data/movies.json' },
+		encoding: { y: { field: 'Major Genre', type: 'nominal', axis: null } },
+		layer: [
+			{
+				mark: { type: 'bar', color: '#ddd' },
+				encoding: {
+					x: {
+						aggregate: 'mean',
+						field: 'IMDB Rating',
+						scale: { domain: [0, 10] },
+						title: 'Mean IMDB Ratings'
+					}
+				}
+			},
+			{
+				mark: { type: 'text', align: 'left', x: 5 },
+				encoding: {
+					text: { field: 'Major Genre' },
+					detail: { aggregate: 'count' }
+				}
+			}
+		]
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
