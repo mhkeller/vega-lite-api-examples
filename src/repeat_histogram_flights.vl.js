7087d3fff9d5452c47e0221b5992aefd6e82a2fe
mhkeller <code@mhkeller.com>
2026-01-26 17:49:21 -0500

    add repeat_histogram_flights

--- src/repeat_histogram_flights.vl.js
+++ src/repeat_histogram_flights.vl.js
@@ -5,6 +5,22 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: { url: 'data/flights-2k.json', format: { parse: { date: 'date' } } },
+		transform: [{ calculate: 'hours(datum.date)', as: 'time' }],
+		repeat: { column: ['distance', 'delay', 'time'] },
+		spec: {
+			mark: { type: 'bar' },
+			encoding: {
+				x: { field: { repeat: 'column' }, bin: { maxbins: 20 } },
+				y: { aggregate: 'count' }
+			}
+		}
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
