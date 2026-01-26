976284d99bd7ec9097df20da28505732e1e9c3e7
mhkeller <code@mhkeller.com>
2026-01-26 15:55:14 -0500

    add line_impute_keyvals

--- src/line_impute_keyvals.vl.js
+++ src/line_impute_keyvals.vl.js
@@ -5,6 +5,32 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: {
+			values: [
+				{ a: 0, b: 28, c: 0 },
+				{ a: 0, b: 91, c: 1 },
+				{ a: 1, b: 43, c: 0 },
+				{ a: 1, b: 55, c: 1 },
+				{ a: 2, b: 81, c: 0 },
+				{ a: 2, b: 53, c: 1 },
+				{ a: 3, b: 19, c: 0 }
+			]
+		},
+		transform: [
+			{ impute: 'b', key: 'a', value: 100, groupby: ['c'], keyvals: [4] }
+		],
+		mark: { type: 'line' },
+		encoding: {
+			x: { field: 'a', type: 'quantitative', scale: { nice: 1 } },
+			y: { field: 'b', type: 'quantitative' },
+			color: { field: 'c', type: 'nominal' }
+		}
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
