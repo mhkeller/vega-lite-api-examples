01de885d56803308de30198b402ba05b3af21fe7
mhkeller <code@mhkeller.com>
2026-01-25 22:17:48 -0500

    add time_parse_local

--- src/time_parse_local.vl.js
+++ src/time_parse_local.vl.js
@@ -5,6 +5,22 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.markPoint()
+		.description('Non-ISO time strings are parsed as local time.')
+		.data({
+			values: [
+				{ date: '10 Oct 2011 22:48:00' },
+				{ date: '11 Oct 2022 23:00:00' }
+			]
+		})
+		.encode(
+			vl.y().timeUnit('hoursminutes').fieldO('date').title('time')
+		)
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
