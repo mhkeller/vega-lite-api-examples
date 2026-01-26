3229815fbb2643f0036427991d02f4cdc33d2f7a
mhkeller <code@mhkeller.com>
2026-01-25 22:31:35 -0500

    add config_numberFormatType_test

--- src/config_numberFormatType_test.vl.js
+++ src/config_numberFormatType_test.vl.js
@@ -5,6 +5,28 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.markPoint({ tooltip: true })
+		.description('Testing global number formatting config')
+		.width(150)
+		.height(150)
+		.data('data/cars.json')
+		.encode(
+			vl.x().fieldT('Year'),
+			vl.y().fieldQ('Miles_per_Gallon'),
+			vl.color().fieldQ('Acceleration'),
+			vl.size().field('Displacement').bin(true).type('quantitative'),
+			vl.facet().field('Weight_in_lbs').bin({ maxbins: 4 }).columns(3)
+		)
+		.config({
+			numberFormat: '1.0',
+			numberFormatType: 'pow',
+			customFormatTypes: true
+		})
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
