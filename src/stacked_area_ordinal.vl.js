0ccbff25f0dd94d950cf583a1b3450a59cc71fca
mhkeller <code@mhkeller.com>
2026-01-25 22:14:43 -0500

    add stacked_area_ordinal

--- src/stacked_area_ordinal.vl.js
+++ src/stacked_area_ordinal.vl.js
@@ -5,6 +5,18 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.markArea({ interpolate: 'monotone' })
+		.data('data/cars.json')
+		.encode(
+			vl.x().timeUnit('year').field('Year'),
+			vl.y().aggregate('sum').field('Weight_in_lbs'),
+			vl.color().fieldO('Cylinders')
+		)
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
