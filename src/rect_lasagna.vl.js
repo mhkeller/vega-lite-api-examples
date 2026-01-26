a9ee29c2bb7d3bb784a72fa53a71b97e47dda013
mhkeller <code@mhkeller.com>
2026-01-26 17:46:03 -0500

    add rect_lasagna

--- src/rect_lasagna.vl.js
+++ src/rect_lasagna.vl.js
@@ -5,6 +5,54 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		transform: [{ filter: "datum.symbol !== 'GOOG'" }],
+		width: 300,
+		height: 100,
+		data: { url: 'data/stocks.csv' },
+		mark: { type: 'rect' },
+		encoding: {
+			x: {
+				timeUnit: 'yearmonthdate',
+				field: 'date',
+				type: 'ordinal',
+				title: 'Time',
+				axis: {
+					format: '%Y',
+					labelAngle: 0,
+					labelOverlap: false,
+					labelColor: {
+						condition: {
+							test: {
+								timeUnit: 'monthdate',
+								field: 'value',
+								equal: { month: 1, date: 1 }
+							},
+							value: 'black'
+						},
+						value: null
+					},
+					tickColor: {
+						condition: {
+							test: {
+								timeUnit: 'monthdate',
+								field: 'value',
+								equal: { month: 1, date: 1 }
+							},
+							value: 'black'
+						},
+						value: null
+					}
+				}
+			},
+			color: { aggregate: 'sum', field: 'price', type: 'quantitative', title: 'Price' },
+			y: { field: 'symbol', type: 'nominal', title: null }
+		}
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
