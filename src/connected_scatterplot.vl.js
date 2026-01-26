8dedb45a7960103eff0522a27c771a1395f8fd4c
mhkeller <code@mhkeller.com>
2026-01-25 21:51:01 -0500

    add connected_scatterplot

--- src/connected_scatterplot.vl.js
+++ src/connected_scatterplot.vl.js
@@ -5,6 +5,18 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.markLine({ point: true })
+		.data('data/driving.json')
+		.encode(
+			vl.x().fieldQ('miles').scale({ zero: false }),
+			vl.y().fieldQ('gas').scale({ zero: false }),
+			vl.order().field('year')
+		)
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
