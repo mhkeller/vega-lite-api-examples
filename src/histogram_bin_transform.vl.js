122408bb17b48025e1ae7cd27e7b15e68a32290e
mhkeller <code@mhkeller.com>
2026-01-25 21:33:40 -0500

    add histogram_bin_transform

--- src/histogram_bin_transform.vl.js
+++ src/histogram_bin_transform.vl.js
@@ -5,6 +5,27 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.markBar()
+		.data('data/movies.json')
+		.transform({
+			bin: true,
+			field: 'IMDB Rating',
+			as: 'binned rating'
+		})
+		.encode(
+			vl
+				.x()
+				.field('binned rating')
+				.title('IMDB Rating (binned)')
+				.bin({ binned: true, step: 1 }),
+			vl.x2().field('binned rating_end'),
+			vl.y().aggregate('count')
+		)
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
