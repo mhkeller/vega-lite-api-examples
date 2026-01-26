51f784efe778df6d07b4a0563c6a936bcec792ad
mhkeller <code@mhkeller.com>
2026-01-26 09:40:13 -0500

    add layer_candlestick

--- src/layer_candlestick.vl.js
+++ src/layer_candlestick.vl.js
@@ -5,6 +5,43 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		width: 400,
+		description:
+			'A candlestick chart inspired by an example in Protovis (http://mbostock.github.io/protovis/ex/candlestick.html)',
+		data: { url: 'data/ohlc.json' },
+		encoding: {
+			x: {
+				field: 'date',
+				type: 'temporal',
+				title: 'Date in 2009',
+				axis: { format: '%m/%d', labelAngle: -45, title: 'Date in 2009' }
+			},
+			y: {
+				type: 'quantitative',
+				scale: { zero: false },
+				axis: { title: 'Price' }
+			},
+			color: {
+				condition: { test: 'datum.open < datum.close', value: '#06982d' },
+				value: '#ae1325'
+			}
+		},
+		layer: [
+			{
+				mark: { type: 'rule' },
+				encoding: { y: { field: 'low' }, y2: { field: 'high' } }
+			},
+			{
+				mark: { type: 'bar' },
+				encoding: { y: { field: 'open' }, y2: { field: 'close' } }
+			}
+		]
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
