02212133f5633943af4492b2e953e40119f949f9
mhkeller <code@mhkeller.com>
2026-01-25 21:52:39 -0500

    add window_rank

--- src/window_rank.vl.js
+++ src/window_rank.vl.js
@@ -5,6 +5,48 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.markLine({ orient: 'vertical' })
+		.title({ text: 'World Cup 2018: Group F Rankings', frame: 'bounds' })
+		.data({
+			values: [
+				{ team: 'Germany', matchday: 1, point: 0, diff: -1 },
+				{ team: 'Mexico', matchday: 1, point: 3, diff: 1 },
+				{ team: 'South Korea', matchday: 1, point: 0, diff: -1 },
+				{ team: 'Sweden', matchday: 1, point: 3, diff: 1 },
+				{ team: 'Germany', matchday: 2, point: 3, diff: 0 },
+				{ team: 'Mexico', matchday: 2, point: 6, diff: 2 },
+				{ team: 'South Korea', matchday: 2, point: 0, diff: -2 },
+				{ team: 'Sweden', matchday: 2, point: 3, diff: 0 },
+				{ team: 'Germany', matchday: 3, point: 3, diff: -2 },
+				{ team: 'Mexico', matchday: 3, point: 6, diff: -1 },
+				{ team: 'South Korea', matchday: 3, point: 3, diff: 0 },
+				{ team: 'Sweden', matchday: 3, point: 6, diff: 3 }
+			]
+		})
+		.transform({
+			sort: [
+				{ field: 'point', order: 'descending' },
+				{ field: 'diff', order: 'descending' }
+			],
+			window: [{ op: 'rank', as: 'rank' }],
+			groupby: ['matchday']
+		})
+		.encode(
+			vl.x().fieldO('matchday'),
+			vl.y().fieldO('rank'),
+			vl
+				.color()
+				.fieldN('team')
+				.scale({
+					domain: ['Germany', 'Mexico', 'South Korea', 'Sweden'],
+					range: ['black', '#127153', '#C91A3C', '#0C71AB']
+				})
+		)
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
