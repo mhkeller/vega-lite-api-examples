614c899493584fc5a5f5589a8d4099e619935c6e
mhkeller <code@mhkeller.com>
2026-01-26 09:07:57 -0500

    add layer_arc_label

--- src/layer_arc_label.vl.js
+++ src/layer_arc_label.vl.js
@@ -5,6 +5,34 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		description: 'A simple pie chart with labels.',
+		data: {
+			values: [
+				{ category: 'a', value: 4 },
+				{ category: 'b', value: 6 },
+				{ category: 'c', value: 10 },
+				{ category: 'd', value: 3 },
+				{ category: 'e', value: 7 },
+				{ category: 'f', value: 8 }
+			]
+		},
+		encoding: {
+			theta: { field: 'value', type: 'quantitative', stack: true },
+			color: { field: 'category', type: 'nominal', legend: null }
+		},
+		layer: [
+			{ mark: { type: 'arc', outerRadius: 80 } },
+			{
+				mark: { type: 'text', radius: 90 },
+				encoding: { text: { field: 'category', type: 'nominal' } }
+			}
+		]
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
