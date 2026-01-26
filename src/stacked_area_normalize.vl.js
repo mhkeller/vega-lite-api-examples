eaeeb1796f9fc9fa9be6eea229a6c100d52a3745
mhkeller <code@mhkeller.com>
2026-01-25 21:50:02 -0500

    add stacked_area_normalize

--- src/stacked_area_normalize.vl.js
+++ src/stacked_area_normalize.vl.js
@@ -5,6 +5,29 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.markArea()
+		.data('data/unemployment-across-industries.json')
+		.width(300)
+		.height(200)
+		.encode(
+			vl
+				.x()
+				.timeUnit('yearmonth')
+				.field('date')
+				.axis({ domain: false, format: '%Y' }),
+			vl
+				.y()
+				.aggregate('sum')
+				.field('count')
+				.axis(null)
+				.stack('normalize'),
+			vl.color().field('series').scale({ scheme: 'category20b' })
+		)
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
