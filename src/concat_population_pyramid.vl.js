1b017624976e9f8bf05f0195d2ddeb1723084ae8
mhkeller <code@mhkeller.com>
2026-01-26 08:49:12 -0500

    add concat_population_pyramid

--- src/concat_population_pyramid.vl.js
+++ src/concat_population_pyramid.vl.js
@@ -5,6 +5,76 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		description: 'A population pyramid for the US in 2000.',
+		data: { url: 'data/population.json' },
+		transform: [
+			{ filter: 'datum.year == 2000' },
+			{ calculate: "datum.sex == 2 ? 'Female' : 'Male'", as: 'gender' }
+		],
+		spacing: 0,
+		hconcat: [
+			{
+				transform: [{ filter: { field: 'gender', equal: 'Female' } }],
+				title: 'Female',
+				mark: { type: 'bar' },
+				encoding: {
+					y: { field: 'age', axis: null, sort: 'descending' },
+					x: {
+						aggregate: 'sum',
+						field: 'people',
+						title: 'population',
+						axis: { format: 's' },
+						sort: 'descending'
+					},
+					color: {
+						field: 'gender',
+						scale: { range: ['#675193', '#ca8861'] },
+						legend: null
+					}
+				}
+			},
+			{
+				width: 20,
+				view: { stroke: null },
+				mark: { type: 'text', align: 'center' },
+				encoding: {
+					y: {
+						field: 'age',
+						type: 'ordinal',
+						axis: null,
+						sort: 'descending'
+					},
+					text: { field: 'age', type: 'quantitative' }
+				}
+			},
+			{
+				transform: [{ filter: { field: 'gender', equal: 'Male' } }],
+				title: 'Male',
+				mark: { type: 'bar' },
+				encoding: {
+					y: {
+						field: 'age',
+						title: null,
+						axis: null,
+						sort: 'descending'
+					},
+					x: {
+						aggregate: 'sum',
+						field: 'people',
+						title: 'population',
+						axis: { format: 's' }
+					},
+					color: { field: 'gender', legend: null }
+				}
+			}
+		],
+		config: { view: { stroke: null }, axis: { grid: false } }
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
