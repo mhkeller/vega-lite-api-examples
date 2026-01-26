c0d6a047980c74d2650b5b994dd2ac361e3f3c6e
mhkeller <code@mhkeller.com>
2026-01-26 08:52:26 -0500

    add facet_custom

--- src/facet_custom.vl.js
+++ src/facet_custom.vl.js
@@ -5,6 +5,30 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.markBar()
+		.description(
+			'A trellis bar chart showing the US population distribution of age groups and gender in 2000.'
+		)
+		.data('data/population.json')
+		.transform(
+			{ filter: 'datum.year == 2000' },
+			{ calculate: "datum.sex == 2 ? 'Female' : 'Male'", as: 'gender' }
+		)
+		.width({ step: 17 })
+		.encode(
+			vl
+				.column()
+				.field('gender')
+				.header({ titleFontSize: 20, labelFontSize: 15 }),
+			vl.y().aggregate('sum').field('people').title('population'),
+			vl.x().fieldO('age'),
+			vl.color().field('gender').scale({ range: ['#675193', '#ca8861'] })
+		)
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
