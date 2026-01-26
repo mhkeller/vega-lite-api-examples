93f788b6d600ee738bea36380a8fbc1d0f98346e
mhkeller <code@mhkeller.com>
2026-01-26 09:40:50 -0500

    add layer_bar_line_union

--- src/layer_bar_line_union.vl.js
+++ src/layer_bar_line_union.vl.js
@@ -5,6 +5,42 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: {
+			values: [
+				{ a: 'A', b: 'B', c: 28 },
+				{ a: 'B', b: 'C', c: 55 },
+				{ a: 'C', b: 'D', c: 43 },
+				{ a: 'D', b: 'E', c: 91 },
+				{ a: 'E', b: 'F', c: 81 },
+				{ a: 'F', b: 'G', c: 53 },
+				{ a: 'G', b: 'H', c: 19 },
+				{ a: 'H', b: 'I', c: 87 },
+				{ a: 'I', b: 'J', c: 52 }
+			]
+		},
+		layer: [
+			{
+				mark: { type: 'bar' },
+				encoding: {
+					x: { field: 'a', type: 'nominal' },
+					y: { field: 'c', type: 'quantitative' }
+				}
+			},
+			{
+				mark: { type: 'line' },
+				encoding: {
+					x: { field: 'b', type: 'nominal' },
+					y: { field: 'c', type: 'quantitative' },
+					color: { value: 'red' }
+				}
+			}
+		]
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
