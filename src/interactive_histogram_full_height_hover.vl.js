3a3efb9616f7860311dee3d4db1e868f7c3a6937
mhkeller <code@mhkeller.com>
2026-01-26 09:06:01 -0500

    add interactive_histogram_full_height_hover

--- src/interactive_histogram_full_height_hover.vl.js
+++ src/interactive_histogram_full_height_hover.vl.js
@@ -5,6 +5,46 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: { url: 'data/movies.json' },
+		layer: [
+			{
+				params: [
+					{
+						name: 'hover',
+						select: {
+							type: 'point',
+							on: 'mouseover',
+							clear: 'mouseout'
+						}
+					}
+				],
+				mark: { type: 'bar', color: '#eee', tooltip: true },
+				encoding: {
+					x: { bin: true, field: 'IMDB Rating' },
+					opacity: {
+						condition: {
+							test: { param: 'hover', empty: false },
+							value: 0.5
+						},
+						value: 0
+					},
+					detail: [{ aggregate: 'count' }]
+				}
+			},
+			{
+				mark: { type: 'bar' },
+				encoding: {
+					x: { bin: true, field: 'IMDB Rating' },
+					y: { aggregate: 'count' }
+				}
+			}
+		]
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
