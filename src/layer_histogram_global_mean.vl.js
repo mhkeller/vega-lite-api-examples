5b02813f625f72756ad534e9f5e5fc7d06235806
mhkeller <code@mhkeller.com>
2026-01-26 09:43:02 -0500

    add layer_histogram_global_mean

--- src/layer_histogram_global_mean.vl.js
+++ src/layer_histogram_global_mean.vl.js
@@ -5,6 +5,30 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: { url: 'data/movies.json' },
+		layer: [
+			{
+				mark: { type: 'bar' },
+				encoding: {
+					x: { field: 'IMDB Rating', bin: true },
+					y: { aggregate: 'count' }
+				}
+			},
+			{
+				mark: { type: 'rule' },
+				encoding: {
+					x: { aggregate: 'mean', field: 'IMDB Rating' },
+					color: { value: 'red' },
+					size: { value: 5 }
+				}
+			}
+		]
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
