107e902b5be7a9653d50b296f72987e8a2c87aaa
mhkeller <code@mhkeller.com>
2026-01-25 22:30:29 -0500

    add layer_ranged_dot

--- src/layer_ranged_dot.vl.js
+++ src/layer_ranged_dot.vl.js
@@ -5,6 +5,48 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return vl
+		.layer(
+			vl.markLine().encode(
+				vl.detail().fieldN('country'),
+				vl.color().value('#db646f')
+			),
+			vl.markPoint({ filled: true }).encode(
+				vl
+					.color()
+					.fieldO('year')
+					.scale({ domain: [1955, 2000], range: ['#e6959c', '#911a24'] })
+					.title('Year'),
+				vl.size().value(100),
+				vl.opacity().value(1)
+			)
+		)
+		.description(
+			"A ranged dot plot that uses 'layer' to convey changing life expectancy for the five most populous countries (between 1955 and 2000)."
+		)
+		.data('data/countries.json')
+		.transform(
+			{
+				filter: {
+					field: 'country',
+					oneOf: ['China', 'India', 'United States', 'Indonesia', 'Brazil']
+				}
+			},
+			{ filter: { field: 'year', oneOf: [1955, 2000] } }
+		)
+		.encode(
+			vl.x().fieldQ('life_expect').title('Life Expectancy (years)'),
+			vl.y().fieldN('country').title('Country').axis({
+				offset: 5,
+				ticks: false,
+				minExtent: 70,
+				domain: false
+			})
+		)
+		.toSpec();
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
