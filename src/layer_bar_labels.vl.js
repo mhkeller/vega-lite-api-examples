c95f4b40e9312c646e9aae76af9d1723cd294762
mhkeller <code@mhkeller.com>
2026-01-26 09:10:49 -0500

    add layer_bar_labels

--- src/layer_bar_labels.vl.js
+++ src/layer_bar_labels.vl.js
@@ -5,6 +5,32 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		description:
+			'Bar chart with text labels. Set domain to make the frame cover the labels.',
+		data: {
+			values: [
+				{ a: 'A', b: 28 },
+				{ a: 'B', b: 55 },
+				{ a: 'C', b: 43 }
+			]
+		},
+		encoding: {
+			y: { field: 'a', type: 'nominal' },
+			x: { field: 'b', type: 'quantitative', scale: { domain: [0, 60] } }
+		},
+		layer: [
+			{ mark: { type: 'bar' } },
+			{
+				mark: { type: 'text', align: 'left', baseline: 'middle', dx: 3 },
+				encoding: { text: { field: 'b', type: 'quantitative' } }
+			}
+		]
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
