84b056d6e4d1761e340f4dcac10ea9c8322c26fe
mhkeller <code@mhkeller.com>
2026-01-26 09:45:37 -0500

    add layer_overlay

--- src/layer_overlay.vl.js
+++ src/layer_overlay.vl.js
@@ -5,6 +5,40 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		layer: [
+			{
+				data: { url: 'data/cars.json' },
+				mark: { type: 'line' },
+				encoding: {
+					x: { field: 'Cylinders', type: 'ordinal' },
+					y: {
+						aggregate: 'max',
+						field: 'Horsepower',
+						type: 'quantitative'
+					},
+					color: { value: 'darkred' }
+				}
+			},
+			{
+				data: { url: 'data/cars.json' },
+				mark: { type: 'line' },
+				encoding: {
+					x: { field: 'Cylinders', type: 'ordinal' },
+					y: {
+						aggregate: 'min',
+						field: 'Horsepower',
+						type: 'quantitative'
+					}
+				}
+			}
+		],
+		config: { line: { point: true } }
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
