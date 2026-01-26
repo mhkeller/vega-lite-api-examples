364a5189126d199ebc57612686c48438e5db835b
mhkeller <code@mhkeller.com>
2026-01-26 08:48:51 -0500

    add concat_marginal_histograms

--- src/concat_marginal_histograms.vl.js
+++ src/concat_marginal_histograms.vl.js
@@ -5,6 +5,60 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: { url: 'data/movies.json' },
+		spacing: 15,
+		bounds: 'flush',
+		vconcat: [
+			{
+				mark: { type: 'bar' },
+				height: 60,
+				encoding: {
+					x: { bin: true, field: 'IMDB Rating', axis: null },
+					y: {
+						aggregate: 'count',
+						scale: { domain: [0, 1000] },
+						title: ''
+					}
+				}
+			},
+			{
+				spacing: 15,
+				bounds: 'flush',
+				hconcat: [
+					{
+						mark: { type: 'rect' },
+						encoding: {
+							x: { bin: true, field: 'IMDB Rating' },
+							y: { bin: true, field: 'Rotten Tomatoes Rating' },
+							color: { aggregate: 'count' }
+						}
+					},
+					{
+						mark: { type: 'bar' },
+						width: 60,
+						encoding: {
+							y: {
+								bin: true,
+								field: 'Rotten Tomatoes Rating',
+								axis: null
+							},
+							x: {
+								aggregate: 'count',
+								scale: { domain: [0, 1000] },
+								title: ''
+							}
+						}
+					}
+				]
+			}
+		],
+		config: { view: { stroke: 'transparent' } }
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
