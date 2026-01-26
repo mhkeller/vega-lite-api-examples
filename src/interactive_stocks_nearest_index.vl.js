57d9e64ac8aac7546d2f43ff958a458f38bac87d
mhkeller <code@mhkeller.com>
2026-01-26 09:42:22 -0500

    add interactive_stocks_nearest_index

--- src/interactive_stocks_nearest_index.vl.js
+++ src/interactive_stocks_nearest_index.vl.js
@@ -5,6 +5,60 @@ import * as vl from 'vega-lite-api';
  * generate and return the vega-lite JSON spec below.
  */
 
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		width: 600,
+		height: 300,
+		data: { url: 'data/stocks.csv' },
+		encoding: { x: { field: 'date', type: 'temporal' } },
+		layer: [
+			{
+				mark: { type: 'line' },
+				encoding: {
+					y: { field: 'price', type: 'quantitative' },
+					color: { field: 'symbol', type: 'nominal' }
+				}
+			},
+			{
+				params: [
+					{
+						name: 'index',
+						select: {
+							type: 'point',
+							encodings: ['x'],
+							on: 'mousemove',
+							nearest: true
+						}
+					}
+				],
+				mark: { type: 'point' },
+				encoding: {
+					y: { field: 'price', type: 'quantitative' },
+					opacity: { value: 0 }
+				}
+			},
+			{
+				transform: [
+					{ filter: { and: ['index.date', { param: 'index' }] } }
+				],
+				mark: { type: 'rule' }
+			},
+			{
+				transform: [
+					{ filter: { and: ['index.date', { param: 'index' }] } }
+				],
+				mark: { type: 'text' },
+				encoding: {
+					y: { value: 10 },
+					text: { field: 'date', type: 'temporal' }
+				}
+			}
+		],
+		config: { text: { align: 'right', dx: -5, dy: 5 } }
+	};
+}
+
 /*
 {
   "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
