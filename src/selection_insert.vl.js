7ebdf7b41e9560060a800e7ca88a2ff642d4a099
mhkeller <code@mhkeller.com>
2026-01-26 21:37:16 -0500

    add selection_insert

--- src/selection_insert.vl.js
+++ src/selection_insert.vl.js
@@ -28,3 +28,30 @@ import * as vl from 'vega-lite-api';
   }
 }
 */
+
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: { url: 'data/cars.json' },
+		params: [
+			{
+				name: 'paintbrush',
+				select: { type: 'point', toggle: false }
+			}
+		],
+		mark: { type: 'circle' },
+		encoding: {
+			x: { field: 'Horsepower', type: 'quantitative' },
+			y: { field: 'Miles_per_Gallon', type: 'quantitative' },
+			color: {
+				condition: {
+					param: 'paintbrush',
+					field: 'Cylinders',
+					type: 'ordinal'
+				},
+				value: 'grey'
+			},
+			size: { value: 75 }
+		}
+	};
+}
