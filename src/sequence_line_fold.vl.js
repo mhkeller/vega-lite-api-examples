7362a4fb46d39704d4d6a8aeab5b592f530a9abb
mhkeller <code@mhkeller.com>
2026-01-26 21:44:59 -0500

    add sequence_line_fold

--- src/sequence_line_fold.vl.js
+++ src/sequence_line_fold.vl.js
@@ -50,3 +50,26 @@ import * as vl from 'vega-lite-api';
   }
 }
 */
+
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		description: 'Plots two functions using a generated sequence.',
+		width: 300,
+		height: 150,
+		data: {
+			sequence: { start: 0, stop: 12.7, step: 0.1, as: 'x' }
+		},
+		transform: [
+			{ calculate: 'sin(datum.x)', as: 'sin(x)' },
+			{ calculate: 'cos(datum.x)', as: 'cos(x)' },
+			{ fold: ['sin(x)', 'cos(x)'] }
+		],
+		mark: { type: 'line' },
+		encoding: {
+			x: { type: 'quantitative', field: 'x' },
+			y: { field: 'value', type: 'quantitative' },
+			color: { field: 'key', type: 'nominal', title: null }
+		}
+	};
+}
