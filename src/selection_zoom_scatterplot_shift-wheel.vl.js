faaf5ebfaa28ec4a1b9d8bd36b682fde24735705
mhkeller <code@mhkeller.com>
2026-01-26 21:44:20 -0500

    add selection_zoom_scatterplot_shift-wheel

--- src/selection_zoom_scatterplot_shift-wheel.vl.js
+++ src/selection_zoom_scatterplot_shift-wheel.vl.js
@@ -31,3 +31,31 @@ import * as vl from 'vega-lite-api';
   }
 }
 */
+
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: { url: 'data/cars.json' },
+		params: [
+			{
+				name: 'grid',
+				select: { type: 'interval', zoom: 'wheel![event.shiftKey]' },
+				bind: 'scales'
+			}
+		],
+		mark: { type: 'circle' },
+		encoding: {
+			x: {
+				field: 'Horsepower',
+				type: 'quantitative',
+				scale: { domain: [75, 150] }
+			},
+			y: {
+				field: 'Miles_per_Gallon',
+				type: 'quantitative',
+				scale: { domain: [20, 40] }
+			},
+			size: { field: 'Cylinders', type: 'quantitative' }
+		}
+	};
+}
