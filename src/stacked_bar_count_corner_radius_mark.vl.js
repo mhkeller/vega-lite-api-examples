b0ba0004c1bcb3e848d75bfea74d39cf435d1350
mhkeller <code@mhkeller.com>
2026-01-26 21:45:45 -0500

    add stacked_bar_count_corner_radius_mark

--- src/stacked_bar_count_corner_radius_mark.vl.js
+++ src/stacked_bar_count_corner_radius_mark.vl.js
@@ -17,3 +17,16 @@ import * as vl from 'vega-lite-api';
   }
 }
 */
+
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: { url: 'data/seattle-weather.csv' },
+		mark: { type: 'bar', cornerRadiusTopLeft: 3, cornerRadiusTopRight: 3 },
+		encoding: {
+			x: { timeUnit: 'month', field: 'date', type: 'ordinal' },
+			y: { aggregate: 'count' },
+			color: { field: 'weather' }
+		}
+	};
+}
