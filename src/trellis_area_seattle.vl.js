0ed5886c0d0b7f8f06cf7b95d5ea1c19c74ae170
mhkeller <code@mhkeller.com>
2026-01-26 21:51:21 -0500

    add trellis_area_seattle

--- src/trellis_area_seattle.vl.js
+++ src/trellis_area_seattle.vl.js
@@ -55,3 +55,52 @@ import * as vl from 'vega-lite-api';
   "config": {"axis": {"grid": false, "domain": false}}
 }
 */
+
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		description: 'Temperature normals in Seattle. Derived from [Seattle Annual Temperate](https://vega.github.io/vega/examples/annual-temperature/) example from the Vega example gallery.',
+		title: 'Seattle Temperature Normals',
+		data: { url: 'data/seattle-weather-hourly-normals.csv' },
+		transform: [{ calculate: '(hours(datum.date) + 18) % 24', as: 'order' }],
+		spacing: { row: 1 },
+		facet: {
+			row: {
+				field: 'date',
+				timeUnit: 'hours',
+				type: 'nominal',
+				sort: { field: 'order' },
+				header: {
+					labelAngle: 0,
+					labelPadding: 2,
+					titlePadding: -4,
+					labelAlign: 'left',
+					labelExpr: "hours(datum.value) == 0 ? 'Midnight' : hours(datum.value) == 12 ? 'Noon' : timeFormat(datum.value, '%I:%M %p')"
+				}
+			}
+		},
+		spec: {
+			width: 800,
+			height: 25,
+			view: { stroke: null },
+			mark: { type: 'area' },
+			encoding: {
+				x: {
+					field: 'date',
+					type: 'temporal',
+					title: 'Month',
+					timeUnit: 'monthdate',
+					axis: { format: '%b' }
+				},
+				y: {
+					field: 'temperature',
+					type: 'quantitative',
+					aggregate: 'mean',
+					scale: { zero: false },
+					axis: { title: null, labels: false, ticks: false }
+				}
+			}
+		},
+		config: { axis: { grid: false, domain: false } }
+	};
+}
