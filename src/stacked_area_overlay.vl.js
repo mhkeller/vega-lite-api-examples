24d66e2c21fc743aa8b834916968f2db3ea36586
mhkeller <code@mhkeller.com>
2026-01-26 21:44:59 -0500

    add stacked_area_overlay

--- src/stacked_area_overlay.vl.js
+++ src/stacked_area_overlay.vl.js
@@ -25,3 +25,21 @@ import * as vl from 'vega-lite-api';
   }
 }
 */
+
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: { url: 'data/population.json' },
+		transform: [
+			{ filter: 'datum.year == 2000' },
+			{ calculate: "datum.sex == 2 ? 'Female' : 'Male'", as: 'gender' }
+		],
+		mark: { type: 'area', line: true },
+		encoding: {
+			y: { aggregate: 'sum', field: 'people' },
+			x: { field: 'age' },
+			color: { field: 'gender', scale: { range: ['#675193', '#ca8861'] } },
+			opacity: { value: 0.7 }
+		}
+	};
+}
