2b9846c9416f9c2b7055ec53e0f802bbbd4d6e3e
mhkeller <code@mhkeller.com>
2026-01-26 21:46:37 -0500

    add stacked_bar_population

--- src/stacked_bar_population.vl.js
+++ src/stacked_bar_population.vl.js
@@ -29,3 +29,22 @@ import * as vl from 'vega-lite-api';
   }
 }
 */
+
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		description: 'A bar chart showing the US population distribution of age groups and gender in 2000.',
+		data: { url: 'data/population.json' },
+		transform: [
+			{ filter: 'datum.year == 2000' },
+			{ calculate: "datum.sex == 2 ? 'Female' : 'Male'", as: 'gender' }
+		],
+		width: { step: 17 },
+		mark: { type: 'bar' },
+		encoding: {
+			y: { aggregate: 'sum', field: 'people', title: 'population' },
+			x: { field: 'age' },
+			color: { field: 'gender', scale: { range: ['#675193', '#ca8861'] } }
+		}
+	};
+}
