1b9ebb7ddfc24f53ef914067bf62b245e5b67739
mhkeller <code@mhkeller.com>
2026-01-26 21:43:32 -0500

    add selection_type_single_mouseover

--- src/selection_type_single_mouseover.vl.js
+++ src/selection_type_single_mouseover.vl.js
@@ -27,3 +27,20 @@ import * as vl from 'vega-lite-api';
   }
 }
 */
+
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: { url: 'data/cars.json' },
+		params: [{ name: 'pts', select: { type: 'point', on: 'mouseover' } }],
+		mark: { type: 'rect' },
+		encoding: {
+			y: { field: 'Origin' },
+			x: { field: 'Cylinders' },
+			color: {
+				condition: { param: 'pts', aggregate: 'count' },
+				value: 'grey'
+			}
+		}
+	};
+}
