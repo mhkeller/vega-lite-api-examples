332db3436152d6eb26f70077f855c09fecfa9061
mhkeller <code@mhkeller.com>
2026-01-26 21:38:29 -0500

    add selection_multi_condition

--- src/selection_multi_condition.vl.js
+++ src/selection_multi_condition.vl.js
@@ -34,3 +34,30 @@ import * as vl from 'vega-lite-api';
   }
 }
 */
+
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		description: 'Drag out a rectangular brush to highlight points.',
+		data: { url: 'data/cars.json' },
+		params: [
+			{ name: 'brush', select: 'interval' },
+			{
+				name: 'hoverbrush',
+				select: { type: 'point', on: 'mouseover', nearest: true }
+			}
+		],
+		mark: { type: 'point' },
+		encoding: {
+			x: { field: 'Horsepower', type: 'quantitative' },
+			y: { field: 'Miles_per_Gallon', type: 'quantitative' },
+			color: {
+				condition: [
+					{ param: 'hoverbrush', value: 'teal' },
+					{ param: 'brush', value: 'skyblue' }
+				],
+				value: 'grey'
+			}
+		}
+	};
+}
