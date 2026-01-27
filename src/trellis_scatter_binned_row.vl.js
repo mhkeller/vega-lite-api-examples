1cf4ee9ea0803d30dd66d238d2db26e6423656c7
mhkeller <code@mhkeller.com>
2026-01-26 21:55:45 -0500

    add trellis_scatter_binned_row

--- src/trellis_scatter_binned_row.vl.js
+++ src/trellis_scatter_binned_row.vl.js
@@ -18,3 +18,17 @@ import * as vl from 'vega-lite-api';
   }
 }
 */
+
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		description: 'A trellis scatterplot showing Horsepower and Miles per gallons, faceted by binned values of Acceleration.',
+		data: { url: 'data/cars.json' },
+		mark: { type: 'point' },
+		encoding: {
+			row: { field: 'Acceleration', bin: true },
+			x: { field: 'Horsepower', type: 'quantitative' },
+			y: { field: 'Miles_per_Gallon', type: 'quantitative' }
+		}
+	};
+}
