d67c0e3774b776115874522d6f6918f80765b5f3
mhkeller <code@mhkeller.com>
2026-01-26 21:33:32 -0500

    add selection_bind_cylyr

--- src/selection_bind_cylyr.vl.js
+++ src/selection_bind_cylyr.vl.js
@@ -36,3 +36,38 @@ import * as vl from 'vega-lite-api';
   }
 }
 */
+
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: { url: 'data/cars.json' },
+		transform: [{ calculate: 'year(datum.Year)', as: 'Year' }],
+		params: [
+			{
+				name: 'CylYr',
+				select: { type: 'point', fields: ['Cylinders', 'Year'] },
+				bind: {
+					Cylinders: { input: 'range', min: 3, max: 8, step: 1 },
+					Year: { input: 'range', min: 1969, max: 1981, step: 1 }
+				}
+			}
+		],
+		mark: { type: 'circle' },
+		encoding: {
+			x: { field: 'Horsepower', type: 'quantitative' },
+			y: { field: 'Miles_per_Gallon', type: 'quantitative' },
+			color: {
+				condition: {
+					param: 'CylYr',
+					field: 'Origin',
+					type: 'nominal'
+				},
+				value: 'grey'
+			},
+			size: {
+				condition: { param: 'CylYr', value: 100 },
+				value: 50
+			}
+		}
+	};
+}
