2d6667e334176a0d456feb4e49343ecc5b43e785
mhkeller <code@mhkeller.com>
2026-01-26 21:46:37 -0500

    add stacked_bar_h_order_custom

--- src/stacked_bar_h_order_custom.vl.js
+++ src/stacked_bar_h_order_custom.vl.js
@@ -22,3 +22,23 @@ import * as vl from 'vega-lite-api';
   }
 }
 */
+
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: { url: 'data/barley.json' },
+		transform: [
+			{
+				calculate: "if(datum.site === 'University Farm', 0, if(datum.site === 'Grand Rapids', 1, 2))",
+				as: 'siteOrder'
+			}
+		],
+		mark: { type: 'bar' },
+		encoding: {
+			x: { aggregate: 'sum', field: 'yield' },
+			y: { field: 'variety' },
+			color: { field: 'site' },
+			order: { field: 'siteOrder' }
+		}
+	};
+}
