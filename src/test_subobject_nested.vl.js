fcdff88385744b4778ae731e67ec6b868e336cd3
mhkeller <code@mhkeller.com>
2026-01-26 21:48:33 -0500

    add test_subobject_nested

--- src/test_subobject_nested.vl.js
+++ src/test_subobject_nested.vl.js
@@ -34,3 +34,33 @@ import * as vl from 'vega-lite-api';
   }
 }
 */
+
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		data: {
+			values: {
+				hits: {
+					hits: [
+						{ source: { reco: 2, yes: 1 } },
+						{ source: { reco: 3, yes: 4 } },
+						{ source: { reco: 2, yes: 0 } },
+						{ source: { reco: 1, yes: 3 } },
+						{ source: { reco: 3, yes: 4 } },
+						{ source: { reco: 1, yes: 1 } },
+						{ source: { reco: 1, yes: 1 } },
+						{ source: { reco: 1, yes: 1 } },
+						{ source: { reco: 1, yes: 0 } },
+						{ source: { reco: 1, yes: 1 } }
+					]
+				}
+			},
+			format: { property: 'hits.hits' }
+		},
+		mark: { type: 'point' },
+		encoding: {
+			x: { field: 'source.reco', type: 'quantitative' },
+			y: { field: 'source.yes', type: 'quantitative' }
+		}
+	};
+}
