5819b693b191a7c9bc7bd38b7f68f8e753572313
mhkeller <code@mhkeller.com>
2026-01-26 21:48:33 -0500

    add test_aggregate_nested

--- src/test_aggregate_nested.vl.js
+++ src/test_aggregate_nested.vl.js
@@ -43,3 +43,25 @@ import * as vl from 'vega-lite-api';
   }
 }
 */
+
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		mark: { type: 'bar' },
+		encoding: {
+			y: { field: 'properties.variety', type: 'nominal' },
+			x: { aggregate: 'sum', field: 'properties.yield', type: 'quantitative' },
+			color: { title: 'site', field: 'properties.site', type: 'nominal' }
+		},
+		data: {
+			values: {
+				features: [
+					{ properties: { variety: 'Manchuria', yield: 27, site: 'University Farm' } },
+					{ properties: { variety: 'Wisconsin No. 38', yield: 29.33333, site: 'Duluth' } }
+				],
+				type: 'FeatureCollection'
+			},
+			format: { type: 'json', property: 'features' }
+		}
+	};
+}
