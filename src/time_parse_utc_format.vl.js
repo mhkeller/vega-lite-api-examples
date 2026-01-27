de9b0d53b4dc1089ba55351a336fdbea4f14622a
mhkeller <code@mhkeller.com>
2026-01-26 21:50:21 -0500

    add time_parse_utc_format

--- src/time_parse_utc_format.vl.js
+++ src/time_parse_utc_format.vl.js
@@ -29,3 +29,21 @@ import * as vl from 'vega-lite-api';
   }
 }
 */
+
+export default function chart() {
+	return {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		description: 'Using format parse to parsing of input time data as utc time.',
+		data: {
+			values: [
+				{ date: '10 Oct 2011 22:48:00' },
+				{ date: '11 Oct 2022 23:00:00' }
+			],
+			format: { parse: { date: "utc:'%d %b %Y %H:%M:%S'" } }
+		},
+		mark: { type: 'point' },
+		encoding: {
+			y: { timeUnit: 'hoursminutes', field: 'date', type: 'ordinal', title: 'time' }
+		}
+	};
+}
