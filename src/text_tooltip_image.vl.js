93ba782eb23d4314440bc2e00cd6733fadeb68e0
mhkeller <code@mhkeller.com>
2026-01-27 23:44:00 -0500

    convert text_tooltip_image, trail_comet to vl API

--- src/text_tooltip_image.vl.js
+++ src/text_tooltip_image.vl.js
@@ -1,9 +1,6 @@
 import * as vl from 'vega-lite-api';
 
-/**
- * Write a Node.JS function that uses the vega-lite-api library to
- * generate and return the vega-lite JSON spec below.
- */
+
 
 /*
 {
@@ -27,22 +24,21 @@ import * as vl from 'vega-lite-api';
 */
 
 export default function chart() {
-	return {
-		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
-		view: { stroke: null },
-		description: 'A simple chart with an image tooltip.',
-		data: {
-			values: [
-				{ image: 'data/ffox.png' },
-				{ image: 'data/gimp.png' },
-				{ image: 'data/7zip.png' }
-			]
-		},
-		mark: { type: 'text' },
-		encoding: {
-			text: { field: 'image' },
-			y: { field: 'image', axis: null },
-			tooltip: [{ field: 'image' }]
-		}
-	};
+	const data = [
+		{ image: 'data/ffox.png' },
+		{ image: 'data/gimp.png' },
+		{ image: 'data/7zip.png' }
+	];
+
+	return vl
+		.markText()
+		.view({ stroke: null })
+		.description('A simple chart with an image tooltip.')
+		.data({ values: data })
+		.encode(
+			vl.text().field('image'),
+			vl.y().field('image').axis(null),
+			vl.tooltip([{ field: 'image' }])
+		)
+		.toSpec();
 }
--- src/trail_comet.vl.js
+++ src/trail_comet.vl.js
@@ -42,37 +42,29 @@ import * as vl from 'vega-lite-api';
 */
 
 export default function chart() {
-	return {
-		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
-		data: { url: 'data/barley.json' },
-		title: 'Barley Yield comparison between 1932 and 1931',
-		transform: [
+	return vl
+		.markTrail()
+		.title('Barley Yield comparison between 1932 and 1931')
+		.data('data/barley.json')
+		.transform(
 			{ pivot: 'year', value: 'yield', groupby: ['variety', 'site'] },
 			{ fold: ['1931', '1932'], as: ['year', 'yield'] },
-			{ calculate: 'toNumber(datum.year)', as: 'year' },
-			{ calculate: "datum['1932'] - datum['1931']", as: 'delta' }
-		],
-		mark: { type: 'trail' },
-		encoding: {
-			x: { field: 'year', title: null },
-			y: { field: 'variety', title: 'Variety' },
-			size: {
-				field: 'yield',
-				type: 'quantitative',
-				scale: { range: [0, 12] },
-				legend: { values: [20, 60] },
-				title: 'Barley Yield (bushels/acre)'
-			},
-			tooltip: [{ field: 'year', type: 'quantitative' }, { field: 'yield' }],
-			color: {
-				field: 'delta',
-				type: 'quantitative',
-				scale: { domainMid: 0 },
-				title: 'Yield Delta (%)'
-			},
-			column: { field: 'site', title: 'Site' }
-		},
-		view: { stroke: null },
-		config: { legend: { orient: 'bottom', direction: 'horizontal' } }
-	};
+			vl.calculate('toNumber(datum.year)').as('year'),
+			vl.calculate("datum['1932'] - datum['1931']").as('delta')
+		)
+		.encode(
+			vl.x().field('year').title(null),
+			vl.y().field('variety').title('Variety'),
+			vl.size()
+				.fieldQ('yield')
+				.scale({ range: [0, 12] })
+				.legend({ values: [20, 60] })
+				.title('Barley Yield (bushels/acre)'),
+			vl.tooltip([{ field: 'year', type: 'quantitative' }, { field: 'yield' }]),
+			vl.color().fieldQ('delta').scale({ domainMid: 0 }).title('Yield Delta (%)'),
+			vl.column().field('site').title('Site')
+		)
+		.view({ stroke: null })
+		.config({ legend: { orient: 'bottom', direction: 'horizontal' } })
+		.toSpec();
 }
