f657de132a442fdeb234a24ae979a10046ec5de4
mhkeller <code@mhkeller.com>
2026-01-27 23:44:50 -0500

    convert test_aggregate_nested to vl API

--- src/test_aggregate_nested.vl.js
+++ src/test_aggregate_nested.vl.js
@@ -1,9 +1,6 @@
 import * as vl from 'vega-lite-api';
 
-/**
- * Write a Node.JS function that uses the vega-lite-api library to
- * generate and return the vega-lite JSON spec below.
- */
+
 
 /*
 {
@@ -45,23 +42,24 @@ import * as vl from 'vega-lite-api';
 */
 
 export default function chart() {
-	return {
-		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
-		mark: { type: 'bar' },
-		encoding: {
-			y: { field: 'properties.variety', type: 'nominal' },
-			x: { aggregate: 'sum', field: 'properties.yield', type: 'quantitative' },
-			color: { title: 'site', field: 'properties.site', type: 'nominal' }
+	const data = {
+		values: {
+			features: [
+				{ properties: { variety: 'Manchuria', yield: 27, site: 'University Farm' } },
+				{ properties: { variety: 'Wisconsin No. 38', yield: 29.33333, site: 'Duluth' } }
+			],
+			type: 'FeatureCollection'
 		},
-		data: {
-			values: {
-				features: [
-					{ properties: { variety: 'Manchuria', yield: 27, site: 'University Farm' } },
-					{ properties: { variety: 'Wisconsin No. 38', yield: 29.33333, site: 'Duluth' } }
-				],
-				type: 'FeatureCollection'
-			},
-			format: { type: 'json', property: 'features' }
-		}
+		format: { type: 'json', property: 'features' }
 	};
+
+	return vl
+		.markBar()
+		.data(data)
+		.encode(
+			vl.y().fieldN('properties.variety'),
+			vl.x().aggregate('sum').fieldQ('properties.yield'),
+			vl.color().fieldN('properties.site').title('site')
+		)
+		.toSpec();
 }
