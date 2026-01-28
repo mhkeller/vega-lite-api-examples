6d77755cc7129eae7d19f7fc32e6b86f55c19e85
mhkeller <code@mhkeller.com>
2026-01-27 23:40:00 -0500

    convert trellis_barley_independent to vl API

--- src/trellis_barley_independent.vl.js
+++ src/trellis_barley_independent.vl.js
@@ -1,9 +1,6 @@
 import * as vl from 'vega-lite-api';
 
-/**
- * Write a Node.JS function that uses the vega-lite-api library to
- * generate and return the vega-lite JSON spec below.
- */
+
 
 /*
 {
@@ -38,29 +35,20 @@ import * as vl from 'vega-lite-api';
 */
 
 export default function chart() {
-	return {
-		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
-		name: 'trellis_barley',
-		description: 'The Trellis display by Becker et al. helped establish small multiples as a \u201Cpowerful mechanism for understanding interactions in studies of how a response depends on explanatory variables\u201D. Here we reproduce a trellis of Barley yields from the 1930s, complete with main-effects ordering to facilitate comparison.',
-		data: { url: 'data/barley.json' },
-		mark: { type: 'point' },
-		height: { step: 12 },
-		encoding: {
-			facet: {
-				field: 'site',
-				type: 'ordinal',
-				columns: 2,
-				sort: { op: 'median', field: 'yield' }
-			},
-			x: {
-				aggregate: 'median',
-				field: 'yield',
-				type: 'quantitative',
-				scale: { zero: false }
-			},
-			y: { field: 'variety', type: 'ordinal', sort: '-x' },
-			color: { field: 'year', type: 'nominal' }
-		},
-		resolve: { axis: { x: 'independent', y: 'independent' } }
-	};
+	return vl
+		.markPoint()
+		.name('trellis_barley')
+		.description(
+			'The Trellis display by Becker et al. helped establish small multiples as a \u201Cpowerful mechanism for understanding interactions in studies of how a response depends on explanatory variables\u201D. Here we reproduce a trellis of Barley yields from the 1930s, complete with main-effects ordering to facilitate comparison.'
+		)
+		.data('data/barley.json')
+		.height({ step: 12 })
+		.encode(
+			vl.facet().fieldO('site').columns(2).sort({ op: 'median', field: 'yield' }),
+			vl.x().aggregate('median').fieldQ('yield').scale({ zero: false }),
+			vl.y().fieldO('variety').sort('-x'),
+			vl.color().fieldN('year')
+		)
+		.resolve({ axis: { x: 'independent', y: 'independent' } })
+		.toSpec();
 }
