cd906d768164de544e4e215f0eeb9b73982e2a18
mhkeller <code@mhkeller.com>
2026-01-27 23:43:30 -0500

    convert time_output_utc_scale, time_output_utc_timeunit, time_parse_utc_format to vl API

--- src/time_output_utc_timeunit.vl.js
+++ src/time_output_utc_timeunit.vl.js
@@ -1,9 +1,6 @@
 import * as vl from 'vega-lite-api';
 
-/**
- * Write a Node.JS function that uses the vega-lite-api library to
- * generate and return the vega-lite JSON spec below.
- */
+
 
 /*
 {
@@ -31,26 +28,21 @@ import * as vl from 'vega-lite-api';
 */
 
 export default function chart() {
-	return {
-		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
-		description: 'Using utc timeunit with local time input.',
-		data: {
-			values: [
-				{ date: 'Sun, 01 Jan 2012 23:00:00', price: 150 },
-				{ date: 'Sun, 02 Jan 2012 00:00:00', price: 100 },
-				{ date: 'Sun, 02 Jan 2012 01:00:00', price: 170 },
-				{ date: 'Sun, 02 Jan 2012 02:00:00', price: 165 },
-				{ date: 'Sun, 02 Jan 2012 03:00:00', price: 200 }
-			]
-		},
-		mark: { type: 'line' },
-		encoding: {
-			x: {
-				field: 'date',
-				timeUnit: 'utcyearmonthdatehoursminutes',
-				axis: { labelAngle: 15 }
-			},
-			y: { field: 'price', type: 'quantitative' }
-		}
-	};
+	const data = [
+		{ date: 'Sun, 01 Jan 2012 23:00:00', price: 150 },
+		{ date: 'Sun, 02 Jan 2012 00:00:00', price: 100 },
+		{ date: 'Sun, 02 Jan 2012 01:00:00', price: 170 },
+		{ date: 'Sun, 02 Jan 2012 02:00:00', price: 165 },
+		{ date: 'Sun, 02 Jan 2012 03:00:00', price: 200 }
+	];
+
+	return vl
+		.markLine()
+		.description('Using utc timeunit with local time input.')
+		.data({ values: data })
+		.encode(
+			vl.x().field('date').timeUnit('utcyearmonthdatehoursminutes').axis({ labelAngle: 15 }),
+			vl.y().fieldQ('price')
+		)
+		.toSpec();
 }
--- src/time_parse_utc_format.vl.js
+++ src/time_parse_utc_format.vl.js
@@ -1,9 +1,6 @@
 import * as vl from 'vega-lite-api';
 
-/**
- * Write a Node.JS function that uses the vega-lite-api library to
- * generate and return the vega-lite JSON spec below.
- */
+
 
 /*
 {
@@ -31,19 +28,18 @@ import * as vl from 'vega-lite-api';
 */
 
 export default function chart() {
-	return {
-		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
-		description: 'Using format parse to parsing of input time data as utc time.',
-		data: {
+	return vl
+		.markPoint()
+		.description('Using format parse to parsing of input time data as utc time.')
+		.data({
 			values: [
 				{ date: '10 Oct 2011 22:48:00' },
 				{ date: '11 Oct 2022 23:00:00' }
 			],
 			format: { parse: { date: "utc:'%d %b %Y %H:%M:%S'" } }
-		},
-		mark: { type: 'point' },
-		encoding: {
-			y: { timeUnit: 'hoursminutes', field: 'date', type: 'ordinal', title: 'time' }
-		}
-	};
+		})
+		.encode(
+			vl.y().timeUnit('hoursminutes').field('date').type('ordinal').title('time')
+		)
+		.toSpec();
 }
