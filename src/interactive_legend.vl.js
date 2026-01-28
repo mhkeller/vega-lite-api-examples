a2c836b3572a3c7132f50b571ed9e127d40414bc
mhkeller <code@mhkeller.com>
2026-01-27 23:12:07 -0500

    Fix interactive_legend and interactive_dashboard_europe_pop to use vl API

--- src/interactive_legend.vl.js
+++ src/interactive_legend.vl.js
@@ -6,38 +6,21 @@ import * as vl from 'vega-lite-api';
  */
 
 export default function chart() {
-	return {
-		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
-		width: 300,
-		height: 200,
-		data: { url: 'data/unemployment-across-industries.json' },
-		mark: { type: 'area' },
-		params: [
-			{
-				name: 'industry',
-				select: { type: 'point', fields: ['series'] },
-				bind: 'legend'
-			}
-		],
-		encoding: {
-			x: {
-				timeUnit: 'yearmonth',
-				field: 'date',
-				axis: { domain: false, format: '%Y', tickSize: 0 }
-			},
-			y: {
-				aggregate: 'sum',
-				field: 'count',
-				stack: 'center',
-				axis: null
-			},
-			color: { field: 'series', scale: { scheme: 'category20b' } },
-			opacity: {
-				condition: { param: 'industry', value: 1 },
-				value: 0.2
-			}
-		}
-	};
+	const industry = vl.selectPoint('industry').fields('series').bind('legend');
+
+	return vl
+		.markArea()
+		.width(300)
+		.height(200)
+		.data('data/unemployment-across-industries.json')
+		.params(industry)
+		.encode(
+			vl.x().timeUnit('yearmonth').field('date').axis({ domain: false, format: '%Y', tickSize: 0 }),
+			vl.y().aggregate('sum').field('count').stack('center').axis(null),
+			vl.color().field('series').scale({ scheme: 'category20b' }),
+			vl.opacity().if(industry, vl.value(1)).value(0.2)
+		)
+		.toSpec();
 }
 
 /*
