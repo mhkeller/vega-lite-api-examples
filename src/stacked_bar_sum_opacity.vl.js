74e795c6abd064a785f4730200f5ca0cb57d5fb6
mhkeller <code@mhkeller.com>
2026-01-27 23:42:52 -0500

    convert stacked_bar_sum_opacity, stacked_bar_unaggregate to vl API

--- src/stacked_bar_sum_opacity.vl.js
+++ src/stacked_bar_sum_opacity.vl.js
@@ -1,9 +1,6 @@
 import * as vl from 'vega-lite-api';
 
-/**
- * Write a Node.JS function that uses the vega-lite-api library to
- * generate and return the vega-lite JSON spec below.
- */
+
 
 /*
 {
@@ -33,20 +30,19 @@ import * as vl from 'vega-lite-api';
 */
 
 export default function chart() {
-	return {
-		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
-		description: 'A bar chart showing the US population distribution of age groups and gender in 2000.',
-		data: { url: 'data/population.json' },
-		transform: [
-			{ filter: 'datum.year == 2000' },
-			{ calculate: "datum.sex == 2 ? 'Female' : 'Male'", as: 'gender' }
-		],
-		width: { step: 17 },
-		mark: { type: 'bar' },
-		encoding: {
-			x: { field: 'age', type: 'ordinal' },
-			y: { aggregate: 'sum', field: 'people', type: 'quantitative', title: 'population' },
-			opacity: { field: 'people', type: 'quantitative' }
-		}
-	};
+	return vl
+		.markBar()
+		.description('A bar chart showing the US population distribution of age groups and gender in 2000.')
+		.width({ step: 17 })
+		.data('data/population.json')
+		.transform(
+			vl.filter('datum.year == 2000'),
+			vl.calculate("datum.sex == 2 ? 'Female' : 'Male'").as('gender')
+		)
+		.encode(
+			vl.x().fieldO('age'),
+			vl.y().aggregate('sum').fieldQ('people').title('population'),
+			vl.opacity().fieldQ('people')
+		)
+		.toSpec();
 }
--- src/stacked_bar_unaggregate.vl.js
+++ src/stacked_bar_unaggregate.vl.js
@@ -1,9 +1,6 @@
 import * as vl from 'vega-lite-api';
 
-/**
- * Write a Node.JS function that uses the vega-lite-api library to
- * generate and return the vega-lite JSON spec below.
- */
+
 
 /*
 {
@@ -26,21 +23,20 @@ import * as vl from 'vega-lite-api';
 */
 
 export default function chart() {
-	return {
-		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
-		data: {
-			values: [
-				{ a: 'A1', b: 28, c: 'x' },
-				{ a: 'A1', b: 23, c: 'y' },
-				{ a: 'A2', b: 18, c: 'x' },
-				{ a: 'A2', b: 21, c: 'y' }
-			]
-		},
-		mark: { type: 'bar' },
-		encoding: {
-			x: { field: 'a', type: 'ordinal' },
-			y: { field: 'b', type: 'quantitative', stack: 'zero' },
-			color: { field: 'c', type: 'nominal' }
-		}
-	};
+	const data = [
+		{ a: 'A1', b: 28, c: 'x' },
+		{ a: 'A1', b: 23, c: 'y' },
+		{ a: 'A2', b: 18, c: 'x' },
+		{ a: 'A2', b: 21, c: 'y' }
+	];
+
+	return vl
+		.markBar()
+		.data({ values: data })
+		.encode(
+			vl.x().fieldO('a'),
+			vl.y().fieldQ('b').stack('zero'),
+			vl.color().fieldN('c')
+		)
+		.toSpec();
 }
